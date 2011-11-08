44ef09a584921309deb891a2ac1dff37926c32e5
yoos <yoos117@gmail.com>
2011-11-08 02:02:36 +0000

    Logfile parser automatically labels title and axes and sets X and Y axis limits.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@300 a504bc44-2baa-4cb8-4136-ba805e94d278

--- drl-sim/atrias/scripts/parse_log.py
+++ drl-sim/atrias/scripts/parse_log.py
@@ -13,24 +13,36 @@ else:
     print("File unspecified. Exiting.")
 
 if (len(sys.argv) > 2):   # Column to plot specified?
-    ylabel = dataList[0][int(sys.argv[2])]
+    colNum = 0
+    for item in dataList[0]:   # Run through label row.
+        dataList[0][colNum] = item.strip()   # Strip whitespace.
+        colNum += 1
+
+    xLabel = "Time (ms)"
+    yLabel = dataList[0][int(sys.argv[2])]   # Set X axis label to whatever the label for index sys.argv[2] is.
+    myTitle = yLabel + " vs. " + xLabel
+
     del dataList[0]   # Delete label row.
     del dataList[-1]   # Delete potentially incomplete last row.
+
     for row in dataList:
-        for colNum in [0, int(sys.argv[2])]:
+        for i in [0, int(sys.argv[2])]:   # Parse only the two necessary columns
+        # TODO: There might be a more elegant way to do this.
             try:
-                row[colNum] = float(row[colNum].strip())
+                row[i] = float(row[i].strip())   # Strip whitespace, then cast to float.
             except:
-                pass
+                print "Cannot convert to float!"
 
-    xdata = [x[0] for x in dataList]   # Timestamp
-    ydata = [x[int(sys.argv[2])] for x in dataList]
+    xData = [x[0] for x in dataList]   # Timestamp.
+    yData = [x[int(sys.argv[2])] for x in dataList]   # Specified column.
+    yMin = min(yData)
+    yMax = max(yData)
 
-    plt.plot(xdata, ydata, linewidth=1.0)
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Motor Angle B')
-    plt.title('Title!')
-    plt.axis([dataList[0][0], dataList[-1][0], -5, 5])
+    plt.plot(xData, yData, linewidth=1.0)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(myTitle)
+    plt.axis([dataList[0][0], dataList[-1][0], yMin-(yMax-yMin)*1.05, yMax+(yMax-yMin)*1.05])   # Assuming timestamps are accurate, set X limits to first and last timestamps and Y limits to the minimum and maximum yData values.
     plt.grid(True)
     plt.show()
 else:
