bfdaa576db651421dbe4e532ebd3e22f11275869
yoos <yoos117@gmail.com>
2011-11-11 22:02:16 +0000

    Try to make data_publisher keep up reasonably will with shm updates.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@302 a504bc44-2baa-4cb8-4136-ba805e94d278

--- drl-sim/atrias_controllers/src/data_publisher.cpp
+++ drl-sim/atrias_controllers/src/data_publisher.cpp
@@ -1,83 +1,98 @@
-#include <atrias_controllers/uspace_kern_shm.h>
-#include <atrias_controllers/AtriasData.h>
+#include <atrias_controllers/data_publisher.h>
 
-#include <rtai_shm.h>
-#include <rtai_nam2num.h>
+void callbackShm (const ros::TimerEvent&) {
+    while (i != shm->io_index) {   // Check if shm has been updated.
+        c_in = &shm->controller_input[i];
+        c_out = &shm->controller_output[i];
 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+        // When adding new fields, make sure to update AtriasData.msg.
+        aData.time = msgNum;
+        aData.body_angle  = c_in->body_angle;
+        aData.motor_angleA = c_in->motor_angleA;
+        aData.motor_angleB = c_in->motor_angleB;
+        aData.leg_angleA = c_in->leg_angleA;
+        aData.leg_angleB = c_in->leg_angleB;
 
-#include <sstream>
+        aData.body_ang_vel = c_in->body_ang_vel;
+        aData.motor_velocityA = c_in->motor_velocityA;
+        aData.motor_velocityB = c_in->motor_velocityB;
+        aData.leg_velocityA = c_in->leg_velocityA;
+        aData.leg_velocityB = c_in->leg_velocityB;
 
-#define QUOTEME_(x) #x
-#define QUOTEME(x) QUOTEME_(x)
+        aData.xPosition = c_in->xPosition;
+        aData.yPosition = c_in->yPosition;
+        aData.zPosition = c_in->zPosition;
+        aData.xVelocity = c_in->xVelocity;
+        aData.yVelocity = c_in->yVelocity;
+        aData.zVelocity = c_in->zVelocity;
 
-static Shm * shm;   // shm interface to kernel.
+        aData.motor_torqueA = c_out->motor_torqueA;
+        aData.motor_torqueB = c_out->motor_torqueB;
+
+        aData.horizontal_velocity = c_in->horizontal_velocity;
+        aData.motor_currentA = c_in->motor_currentA;
+        aData.motor_currentB = c_in->motor_currentB;
+        aData.toe_switch = c_in->toe_switch;
+        aData.command = c_in->command;
+
+        // Publish the data. This takes up the majority of the loop time.
+        data_publisher.publish(aData);
+
+        i = (i++) % SHM_TO_USPACE_ENTRIES;
+        msgNum++;
+        ROS_INFO("%d %d", i, shm->io_index);
+    }
+        ROS_INFO("shm not updated yet!");
+}
 
 int main (int argc, char **argv) {
     ros::init(argc, argv, "data_publisher");
-    ros::NodeHandle nh;
-    ros::Publisher data_publisher = nh.advertise<atrias_controllers::AtriasData>("datalog_downlink", 1000);   // Advertise topic at "data_downlink" and queue up to 1000 messages before dropping the oldest ones.
-    ros::Rate loop_rate(3000);
 
-    // Connect to kernel's shm.
-    rt_allow_nonroot_hrt();   // Allow rt_shm_alloc to be used without root permissions.
+    // Initialize the nodehandle. This must be called after ros::init.
+    ros::NodeHandle nh;
+
+    // Advertise the Publisher.
+    //
+    // atrias_controllers::AtriasData - Format of ROS msg to publish.
+    // "datalog_downlink" - Name of topic to publish. ROS Subscribers can
+    //                      listen to what this Publisher publishes by
+    //                      subscribing to this topic.
+    // 1000 - Number of ROS msgs to queue up before dropping the oldest ones.
+    data_publisher = nh.advertise<atrias_controllers::AtriasData>("datalog_downlink", 0);
+
+    // Initialize the Timer. (NOTE: This is NOT a replacement for realtime.
+    // There are no guarantees about how accurate this is.)
+    //
+    // ros::Duration - ROS primitive type representing a period of Time.
+    //                 i.e., Time is a specific moment (e.g., 5:00 pm) whereas
+    //                 Duration is a period of Time (e.g., 5 hours).
+    // 0.002 - 2 milliseconds. The timer duration shouldn't be too critical, as
+    //         callbackShm uses a while (not if) loop to check for io_index
+    //         updates.
+    // callbackShm - function to call every duration..
+    timer = nh.createTimer(ros::Duration(0.002), callbackShm);
+
+    // Allow rt_shm_alloc to be used without root permissions.
+    rt_allow_nonroot_hrt();
+
+    // Attempt to allocate shared memory space.
+    //
+    // rt_shm_alloc() - Allocation function declared in rtai_shm.h.
+    // SHM_NAME - Defined in uspace_kern_shm.h
+    // 0 - Allocation size can be specified, but USE_VMALLOC makes this
+    //     unnecessary.
+    // USE_VMALLOC - A define passed to rt_shm_alloc for.. I don't know.
+    //               TODO: Need to understand this better.
     if (!(shm = (Shm*) rt_shm_alloc(nam2num(SHM_NAME), 0, USE_VMALLOC)))
         ROS_ERROR("rtai_malloc() data to user space failed (maybe /dev/rtai_shm is missing)!");
         
     ROS_INFO("Datalog publisher connected to shm.");
 
-    ControllerInput* c_in;
-    ControllerOutput* c_out;
-    int i = 0;
-    int msgNum = 0;
-    atrias_controllers::AtriasData aData;
+    ros::spin();
 
-    while (ros::ok()) {
-        if (i != shm->io_index) {   // Check if shm has been updated.
-            c_in = &shm->controller_input[i];
-            c_out = &shm->controller_output[i];
+    // Disconnect from shm.
+    rt_shm_free(nam2num(SHM_NAME));
 
-            // When adding new fields, make sure to update AtriasData.msg.
-            aData.time = msgNum;
-            aData.body_angle  = c_in->body_angle;
-            aData.motor_angleA = c_in->motor_angleA;
-            aData.motor_angleB = c_in->motor_angleB;
-            aData.leg_angleA = c_in->leg_angleA;
-            aData.leg_angleB = c_in->leg_angleB;
-
-            aData.body_ang_vel = c_in->body_ang_vel;
-            aData.motor_velocityA = c_in->motor_velocityA;
-            aData.motor_velocityB = c_in->motor_velocityB;
-            aData.leg_velocityA = c_in->leg_velocityA;
-            aData.leg_velocityB = c_in->leg_velocityB;
-
-            aData.xPosition = c_in->xPosition;
-            aData.yPosition = c_in->yPosition;
-            aData.zPosition = c_in->zPosition;
-            aData.xVelocity = c_in->xVelocity;
-            aData.yVelocity = c_in->yVelocity;
-            aData.zVelocity = c_in->zVelocity;
-
-            aData.motor_torqueA = c_out->motor_torqueA;
-            aData.motor_torqueB = c_out->motor_torqueB;
-
-            aData.horizontal_velocity = c_in->horizontal_velocity;
-            aData.motor_currentA = c_in->motor_currentA;
-            aData.motor_currentB = c_in->motor_currentB;
-            aData.toe_switch = c_in->toe_switch;
-            aData.command = c_in->command;
-
-            data_publisher.publish(aData);
-
-            i = (i++) % SHM_TO_USPACE_ENTRIES;
-            msgNum++;
-        }
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-
-    rt_shm_free(nam2num(SHM_NAME));   // Disconnect from shm.
     return 0;
 }
 
