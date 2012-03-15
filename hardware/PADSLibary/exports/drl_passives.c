10961e53e0fc991b5106262973651e806460464b
jesse.grimes.1@gmail.com <jesse.grimes.1@gmail.com@a504bc44-2baa-4cb8-4136-ba805e94d278>
2012-03-15 19:29:01 +0000

    git-svn-id: https://atrias.googlecode.com/svn/trunk@922 a504bc44-2baa-4cb8-4136-ba805e94d278

---
+++ hardware/PADSLibary/exports/drl_passives.c
@@ -0,0 +1,324 @@
+*PADS-LIBRARY-SCH-DECALS-V2007*
+
+CAP-SMALL        32000 32000 100 10 100 10 4 2 0 2 26
+TIMESTAMP 2010.04.24.23.50.38
+"Default Font"
+"Default Font"
+50    -70   0 0 100 10 "Default Font"
+REF-DES
+210   -40   0 2 100 10 "Default Font"
+PART-TYPE
+50    -180  0 2 100 10 "Default Font"
+VALUE
+60    -340  0 2 100 10 "Default Font"
+*
+OPEN   2 1 0 -1
+-50   -100 
+50    -100 
+OPEN   2 1 0 -1
+-50   -150 
+50    -150 
+T0     0     0 0 20    -90   0 0 -20   -40   0 18 PINVRTS
+P-110  -140  0 2 -30   14    0 2 0
+T0     -250  0 4 20    -80   0 0 -20   -40   0 18 PINVRTS
+P-110  -130  0 2 -30   14    0 2 0
+
+CAPA-SMALL       32000 32000 100 10 100 10 4 2 1 2 26
+TIMESTAMP 2010.04.24.23.50.38
+"Default Font"
+"Default Font"
+140   -90   0 0 100 10 "Default Font"
+REF-DES
+240   -30   0 2 100 10 "Default Font"
+PART-TYPE
+140   -130  0 2 100 10 "Default Font"
+VALUE
+140   -240  0 2 100 10 "Default Font"
+*
+OPEN   2 1 0 -1
+-50   -100 
+50    -100 
+OPEN   2 1 0 -1
+-50   -150  133.3 -86.6 -72   -276  72    -132 
+50    -150 
+50     -80    0 0   56   10 0 0 0 0 "Regular Verdana"
++
+T0     0     0 0 20    -80   0 0 -20   -40   0 18 PINVRTS
+P-76   -112  0 2 -30   14    0 2 0
+T0     -250  0 4 20    -80   0 0 -20   -40   0 18 PINVRTS
+P-72   -136  0 2 -30   14    0 2 0
+
+CAPAH-SMALL      32000 32000 100 10 100 10 4 2 1 2 26
+TIMESTAMP 2010.04.24.23.50.38
+"Default Font"
+"Default Font"
+130   130   0 4 100 10 "Default Font"
+REF-DES
+250   250   0 4 100 10 "Default Font"
+PART-TYPE
+130   -80   0 6 100 10 "Default Font"
+VALUE
+120   -190  0 6 100 10 "Default Font"
+*
+OPEN   2 10 0 -1
+100   50   
+100   -50  
+OPEN   2 10 0 -1
+150   50    127.2 105.6 126   -62   250   62   
+150   -50  
+60     30     0 0   56   10 0 0 0 0 "Regular Verdana"
++
+T0     0     0 0 30    20    0 2 170   -30   0 16 PINSHORT
+P-28   -104  0 2 -70   10    0 2 0
+T250   0     0 2 30    20    0 2 170   -30   0 16 PINSHORT
+P-28   -104  0 2 -70   10    0 2 0
+
+CAPH-SMALL       32000 32000 100 10 100 10 4 2 0 2 26
+TIMESTAMP 2010.04.24.23.50.38
+"Default Font"
+"Default Font"
+120   100   0 4 100 10 "Default Font"
+REF-DES
+250   250   0 4 100 10 "Default Font"
+PART-TYPE
+130   -60   0 6 100 10 "Default Font"
+VALUE
+140   -160  0 6 100 10 "Default Font"
+*
+OPEN   2 10 0 -1
+100   50   
+100   -50  
+OPEN   2 10 0 -1
+150   50   
+150   -50  
+T0     0     0 0 80    20    0 2 170   -30   0 16 PINSHORT
+P-40   -110  0 2 -70   10    0 2 0
+T250   0     0 2 80    20    0 2 170   -30   0 16 PINSHORT
+P-40   -120  0 2 -70   10    0 2 0
+
+INDUCTOR         33200 32874 100 10 100 10 4 3 0 2 0
+TIMESTAMP 2010.04.24.01.52.57
+"Default Font"
+"Default Font"
+250   300   0 4 100 10 "Default Font"
+REF-DES
+250   250   0 6 100 10 "Default Font"
+PART-TYPE
+250   -100  0 6 100 10 "Default Font"
+VALUE
+250   -200  0 6 100 10 "Default Font"
+SPIL PART#
+OPEN   2 10 0 -1
+100   0     177.7 -175.4 100   -52   200   48   
+200   0    
+OPEN   2 10 0 -1
+200   0     177.7 -175.4 200   -52   300   48   
+300   0    
+OPEN   2 10 0 -1
+300   0     177.7 -175.4 300   -52   400   48   
+400   0    
+T0     0     0 0 70    30    0 2 170   -30   0 16 PINSHORT
+P-500  10    0 2 0     0     0 0 128
+T500   0     0 2 60    30    0 2 170   -30   0 16 PINSHORT
+P-500  10    0 2 0     0     0 0 128
+
+LED              32000 32000 100 10 100 10 4 8 0 2 24
+TIMESTAMP 2010.04.24.01.52.17
+"Default Font"
+"Default Font"
+100   200   0 4 100 10 "Default Font"
+REF-DES
+250   -150  0 6 100 10 "Default Font"
+PART-TYPE
+250   -250  0 6 100 10 "Default Font"
+*
+250   -350  0 6 100 10 "Default Font"
+*
+OPEN   2 10 0 -1
+90    0    
+390   0    
+OPEN   2 10 0 -1
+340   90   
+340   -90  
+COPCLS 4 10 0 -1
+190   0    
+190   -90  
+340   0    
+190   0    
+COPCLS 4 10 0 -1
+190   0    
+190   90   
+340   0    
+190   0    
+OPEN   2 10 0 -1
+250   150  
+350   250  
+COPCLS 5 10 0 -1
+350   250  
+290   230  
+320   190  
+330   180  
+350   250  
+OPEN   2 10 0 -1
+350   150  
+450   250  
+COPCLS 5 10 0 -1
+460   260  
+400   240  
+430   200  
+440   190  
+460   260  
+T500   0     0 2 130   14    0 2 170   -30   0 16 PINSHORT
+P-500  10    0 2 0     0     0 0 128
+T0     0     0 0 130   14    0 2 170   -30   0 16 PINSHORT
+P-500  10    0 2 0     0     0 0 128
+
+LED-V            32000 32000 100 10 100 10 4 8 0 2 24
+TIMESTAMP 2010.04.24.01.52.17
+"Default Font"
+"Default Font"
+-300  350   0 4 100 10 "Default Font"
+REF-DES
+350   250   0 6 100 10 "Default Font"
+PART-TYPE
+350   150   0 6 100 10 "Default Font"
+COLOR
+350   50    0 6 100 10 "Default Font"
+PART NUMBER
+OPEN   2 10 0 -1
+0     190  
+0     490  
+COPCLS 4 10 0 -1
+0     290  
+-90   290  
+0     440  
+0     290  
+COPCLS 4 10 0 -1
+0     290  
+90    290  
+0     440  
+0     290  
+OPEN   2 10 0 -1
+90    440  
+-90   440  
+OPEN   2 10 0 -1
+76    376  
+176   476  
+COPCLS 5 10 0 -1
+176   476  
+116   456  
+146   416  
+156   406  
+176   476  
+OPEN   2 10 0 -1
+176   376  
+276   476  
+COPCLS 5 10 0 -1
+286   486  
+226   466  
+256   426  
+266   416  
+286   486  
+T0     600   90 4 100   100   90 2 230   0     0 16 PIN
+P-180  470   90 2 0     0     0 0 128
+T0     100   90 0 90    100   90 2 230   0     0 16 PIN
+P-160  360   90 2 0     0     0 0 128
+
+RESB-H-SMALL     32000 32000 100 10 100 10 4 1 0 2 18
+TIMESTAMP 2010.04.24.23.53.59
+"Default Font"
+"Default Font"
+300   80    0 4 100 10 "Default Font"
+REF-DES
+300   180   0 4 100 10 "Default Font"
+PART-TYPE
+310   -120  0 12 100 10 "Default Font"
+VALUE
+300   -80   0 6 100 10 "Default Font"
+*
+CLOSED 5 10 0 -1
+100   50   
+100   -50  
+300   -50  
+300   50   
+100   50   
+T0     0     0 0 34    16    0 0 170   -30   0 16 PINSHORT
+P-160  0     0 2 -70   10    0 2 0
+T400   0     0 2 34    16    0 0 170   -30   0 16 PINSHORT
+P-160  0     0 2 -70   10    0 2 0
+
+RESB-V-SMALL     32000 32000 100 10 100 10 4 1 0 2 18
+TIMESTAMP 2010.04.24.23.53.59
+"Default Font"
+"Default Font"
+140   -260  0 0 100 10 "Default Font"
+REF-DES
+210   -80   0 2 100 10 "Default Font"
+PART-TYPE
+140   -290  0 2 100 10 "Default Font"
+VALUE
+180   -400  0 2 100 10 "Default Font"
+*
+CLOSED 5 10 0 -1
+-50   -100 
+-50   -250 
+50    -250 
+50    -100 
+-50   -100 
+T0     0     90 4 90    -30   90 2 170   -30   0 16 PINSHORT
+P-10   -190  90 2 -70   10    0 2 0
+T0     -350  90 0 80    -30   90 2 170   -30   0 16 PINSHORT
+P-10   -160  90 2 -70   10    0 2 0
+
+RESZ-H-SMALL     32000 32000 100 10 100 10 4 1 0 2 18
+TIMESTAMP 2010.04.24.23.53.59
+"Default Font"
+"Default Font"
+300   100   0 4 100 10 "Default Font"
+REF-DES
+310   200   0 4 100 10 "Default Font"
+PART-TYPE
+300   -100  0 6 100 10 "Default Font"
+VALUE
+300   -200  0 6 100 10 "Default Font"
+*
+OPEN   7 10 0 -1
+100   0    
+120   20   
+160   -20  
+200   20   
+240   -20  
+280   20   
+300   0    
+T0     0     0 0 34    16    0 0 170   -30   0 16 PINSHORT
+P-80   -30   0 2 -70   10    0 2 0
+T400   0     0 2 0     20    0 0 170   -30   0 16 PINSHORT
+P-90   -20   0 2 -70   10    0 2 0
+
+RESZ-V-SMALL     32000 32000 100 10 100 10 4 1 0 2 18
+TIMESTAMP 2010.04.24.23.53.59
+"Default Font"
+"Default Font"
+100   -280  0 0 100 10 "Default Font"
+REF-DES
+100   -180  0 0 100 10 "Default Font"
+PART-TYPE
+100   -320  0 2 100 10 "Default Font"
+VALUE
+100   -420  0 2 100 10 "Default Font"
+*
+OPEN   7 10 0 -1
+0     -100 
+-20   -120 
+20    -160 
+-20   -200 
+20    -240 
+-20   -280 
+0     -300 
+T0     0     0 0 20    -80   0 0 -20   -40   0 18 PINVRTS
+P840   80    0 2 -30   14    0 2 0
+T0     -400  0 4 20    -80   0 0 -20   -40   0 18 PINVRTS
+P840   40    0 2 -30   14    0 2 0
+
+
+*END*
---
+++ hardware/PADSLibary/exports/drl_passives.p
@@ -0,0 +1,73 @@
+*PADS-LIBRARY-PART-TYPES-V2007*
+
+CAPACITOR-SMALL 0805:0603 I CAP 9 1 0 0 0
+TIMESTAMP 2010.04.24.23.50.36
+"Voltage Rating" 
+"Tolerance" 
+"Value" 10uF
+"Manufacturer #1" 
+"Part Number" 
+"Description" THROUGH-HOLE CAPACITOR 0.100 INCHES
+"Sim.Analog.Prefix" 
+"Sim.Analog.Order" Model$
+"Sim.Analog.Model" 
+GATE 4 2 0
+CAP-SMALL
+CAPA-SMALL
+CAPH-SMALL
+CAPAH-SMALL
+1 1 U
+2 1 U
+
+INDUCTOR 0603 I IND 8 1 0 0 0
+TIMESTAMP 2010.04.24.01.52.57
+"Spil Part #" 
+"Manufacturer #1" 
+"Value" Ferrite
+"Cost" 
+"Description" 
+"Sim.Analog.Prefix" L
+"Sim.Analog.Order" Model$
+"Sim.Analog.Model" 
+GATE 1 2 0
+INDUCTOR
+1 0 U
+2 0 U
+
+LED 0603-POLERIZED I DIO 9 1 0 0 0
+TIMESTAMP 2010.04.24.01.52.17
+"Manufacturer #1" 
+"Color" Green
+"Description" LIGHT EMITTING DIODE
+"Sim.Analog.Prefix" D
+"Sim.Analog.Order" Model$
+"Sim.Analog.Model" 
+"MFG #1" 
+"PART DESC" 
+"PART NUMBER" 
+GATE 2 2 0
+LED
+LED-V
+1 0 U
+2 0 U
+
+RESISTOR-SMALL 0805:0603 I RES 8 1 0 0 0
+TIMESTAMP 2010.04.24.23.53.59
+"Tolerance" 
+"Manufacturer #1" 
+"Value" 681
+"Part Number" 
+"Description" RES BODY:150  CENTERS:600
+"Sim.Analog.Prefix" R
+"Sim.Analog.Order" Model$
+"Sim.Analog.Model" 
+GATE 4 2 1
+RESZ-H-SMALL
+RESZ-V-SMALL
+RESB-H-SMALL
+RESB-V-SMALL
+1 1 U
+2 1 U
+
+
+*END*
