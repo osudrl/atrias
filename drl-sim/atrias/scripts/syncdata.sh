268b488c0e4f5fe3b5b6ee9d0a15d7ff7b3c5671
yoos <yoos117@gmail.com>
2012-04-24 18:16:23 +0000

    Minor change.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@1027 a504bc44-2baa-4cb8-4136-ba805e94d278

--- drl-sim/atrias/scripts/syncdata.sh
+++ drl-sim/atrias/scripts/syncdata.sh
@@ -4,5 +4,10 @@
 # This does not delete any files from the local directory even if the files are
 # deleted on zotac-3.
 
-rsync -avz -e ssh drl@zotac-3:atrias/drl-sim/atrias/log_files/*.log `rospack find atrias`/log_files
+if [ $HOSTNAME = "drl-sim" ]; then
+    rsync -avz -e ssh drl@zotac-3:atrias/drl-sim/atrias/log_files/*.log `rospack find atrias`/log_files
+else
+    # TODO: This script should be able to work from anywhere.
+    echo "This script should be run from the desktop!"
+fi
 
