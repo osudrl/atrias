c69ce318a4babe51f85aa9cf6c7780c545a00756
yoos <yoos117@gmail.com>
2012-07-03 20:09:53 +0000

    More demo controller bug-fixes
    git-svn-id: https://atrias.googlecode.com/svn/trunk@1410 a504bc44-2baa-4cb8-4136-ba805e94d278

--- drl-sim/atrias_controllers/ac_demo/src/controller_core.cpp
+++ drl-sim/atrias_controllers/ac_demo/src/controller_core.cpp
@@ -63,15 +63,11 @@ void controllerUpdate(robot_state state, ByteArray input, ControllerOutput *outp
 		if (currentDemo == DEMO3)
 			desiredPos = demo3(state, id, output, elapsedTime);
 		if (currentDemo == DEMO4)
-			desiredPos = demo4(state, id, output, elapsedTime);
+			desiredPos = demo4(state, id, output, 1.);
 		if (currentDemo == DEMO5)
 			desiredPos = demo5(state, id, output, elapsedTime);
 		if (currentDemo == DEMO6)
 			desiredPos = demo6(state, id, output, elapsedTime);
-		
-		if ((uint32_t)(elapsedTime * 1000) % 1000 == 0)	
-			printf("Desired position leg length: %f\n", desiredPos.leg_len);
-
 
 		// If we are currently running a demo and the stop demo button is pressed, go to demo_stopping
 		if (id->commandedState == 0)
@@ -80,36 +76,36 @@ void controllerUpdate(robot_state state, ByteArray input, ControllerOutput *outp
 			currentState = DEMO_STATE_STOPPING;
 			elapsedTime = 0.0;
 		}
-		else
-		{
-			// This is a hack, but for DEMO4, the force controller demo, we don't want to use this position controller code. Instead the demo will directly set desiredTorque variables
-			if (currentDemo == DEMO4)
-			{
-				output->motor_torqueA = desTorqueA;
-				output->motor_torqueB = desTorqueB;
-			}
-			else
-			{
-				float des_mtr_angA = desiredPos.leg_ang - PI + acos(desiredPos.leg_len);
-				float des_mtr_angB = desiredPos.leg_ang + PI - acos(desiredPos.leg_len);
-
-				float des_mtr_ang_velA = -desiredPos.leg_len_vel / 2. / sin(des_mtr_angA - des_mtr_angB) - desiredPos.leg_ang_vel / 2.;
-				float des_mtr_ang_velB = desiredPos.leg_len_vel / 2. / sin(des_mtr_angA - des_mtr_angB) - desiredPos.leg_ang_vel / 2.;
-
-				output->motor_torqueA = 1000. * (des_mtr_angA - state.motor_angleA)
-					+ 20. * (des_mtr_ang_velA - state.motor_velocityA);
-				output->motor_torqueB = 1000. * (des_mtr_angB - state.motor_angleB)
-					+ 20. * (des_mtr_ang_velB - state.motor_velocityB);
-			}
-		}
-
-		output->motor_torque_hip = 2000. * (desiredPos.hip_ang - state.motor_angle_hip)
-			+ 20. * (desiredPos.hip_ang_vel - state.motor_velocity_hip);
 	}
 
 	else if (currentState == DEMO_STATE_STOPPING)
 	{
 		stop_demo(state, id, output);
+	}
+	
+	// This is a hack, but for DEMO4, the force controller demo, we don't want to use this position controller code. Instead the demo will directly set desiredTorque variables
+	if (currentDemo == DEMO4 && currentState == DEMO_STATE_RUNNING)
+	{
+		output->motor_torqueA = desTorqueA;
+		output->motor_torqueB = desTorqueB;
+		output->motor_torque_hip = 2000. * (desiredPos.hip_ang - state.motor_angle_hip)
+			+ 20. * (desiredPos.hip_ang_vel - state.motor_velocity_hip);
+	}
+	else if (currentState != DEMO_STATE_STOPPED)
+	{
+		float des_mtr_angA = desiredPos.leg_ang - PI + acos(desiredPos.leg_len);
+		float des_mtr_angB = desiredPos.leg_ang + PI - acos(desiredPos.leg_len);
+
+		float des_mtr_ang_velA = -desiredPos.leg_len_vel / 2. / sin(des_mtr_angA - des_mtr_angB) - desiredPos.leg_ang_vel / 2.;
+		float des_mtr_ang_velB = desiredPos.leg_len_vel / 2. / sin(des_mtr_angA - des_mtr_angB) - desiredPos.leg_ang_vel / 2.;
+
+		output->motor_torqueA = 1000. * (des_mtr_angA - state.motor_angleA)
+			+ 20. * (des_mtr_ang_velA - state.motor_velocityA);
+		output->motor_torqueB = 1000. * (des_mtr_angB - state.motor_angleB)
+			+ 20. * (des_mtr_ang_velB - state.motor_velocityB);
+	
+		output->motor_torque_hip = 2000. * (desiredPos.hip_ang - state.motor_angle_hip)
+			+ 20. * (desiredPos.hip_ang_vel - state.motor_velocity_hip);
 	}
 }
 
@@ -138,7 +134,6 @@ void start_demo(robot_state& state, InputData* id, ControllerOutput* output)
 	if (elapsedTime >= START_TIME)
 	{
 		elapsedTime = 0.;
-		printf("Controller has started!\n");
 		currentState = DEMO_STATE_RUNNING;
 	}
 }
@@ -249,7 +244,7 @@ RobotPosition demo4(robot_state& state, InputData* id, ControllerOutput* output,
 
 	// If outside of safety limits, Engage damping controller
 	// Logic:  if value is not equal to its "CLAMPED" value, it must be out of range
-	if(LEG_LENGTH(state.motor_angleA, state.motor_angleB) != CLAMP(LEG_LENGTH(state.motor_angleA, state.motor_angleB), safety_length_short, safety_length_long) ||
+	if(leg_length != CLAMP(leg_length, safety_length_short, safety_length_long) ||
 		state.motor_angleA != CLAMP(state.motor_angleA, safety_angleA_short, safety_angleA_long) ||
 		state.motor_angleB != CLAMP(state.motor_angleB, safety_angleB_short, safety_angleB_long))
 	{
@@ -276,23 +271,23 @@ RobotPosition demo4(robot_state& state, InputData* id, ControllerOutput* output,
 		float GEAR_RATIO = 50.;
 
 		// Using the current leg configuration, converts the desired axial force into a desired leg torque
-		float desired_torque = 0.5*desired_force*cos(PI + asin(LEG_LENGTH(state.motor_angleA, state.motor_angleB)));
+		float desired_torque = 0.5*desired_force*cos(PI + asin(leg_length));
 
 		// Inverse spring function:  Converts desired torques into desired deflection angles.
 		//		float desired_spring_def = desired_torque/(MOTOR_CONSTANT*SPRING_CONSTANT*GEAR_RATIO);
 		float desired_spring_def = desired_torque/(SPRING_CONSTANT);
 
 		// NOTE: defA is NEGATIVE when compressed.  Should be opposite sign of defB.
-		state.desired_spring_defA = desired_spring_def;
-		state.desired_spring_defB = -1.0*desired_spring_def;
+		float desired_spring_defA = desired_spring_def;
+		float desired_spring_defB = -1.0*desired_spring_def;
 
 		// Leg A, PD control.
-		state.desired_motor_angleA = state.motor_angleA + (state.desired_spring_defA - spring_defA);
-		desTorqueA = (FORCE_CONTROLLER_P_GAIN * (state.desired_motor_angleA - state.motor_angleA)) - (FORCE_CONTROLLER_D_GAIN * (state.motor_velocityA));
+		float desired_motor_angleA = state.motor_angleA + (desired_spring_defA - spring_defA);
+		desTorqueA = (FORCE_CONTROLLER_P_GAIN * (desired_motor_angleA - state.motor_angleA)) - (FORCE_CONTROLLER_D_GAIN * (state.motor_velocityA));
 
 		// Leg B, PD control.
-		state.desired_motor_angleB = state.motor_angleB + (state.desired_spring_defB - spring_defB);
-		desTorqueB = (FORCE_CONTROLLER_P_GAIN * (state.desired_motor_angleB - state.motor_angleB)) - (FORCE_CONTROLLER_D_GAIN * (state.motor_velocityB));
+		float desired_motor_angleB = state.motor_angleB + (desired_spring_defB - spring_defB);
+		desTorqueB = (FORCE_CONTROLLER_P_GAIN * (desired_motor_angleB - state.motor_angleB)) - (FORCE_CONTROLLER_D_GAIN * (state.motor_velocityB));
 	}
 
 	RobotPosition desPos;
