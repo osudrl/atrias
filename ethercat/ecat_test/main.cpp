0883d76b4abbbe85ddb449799fe8aff891328f5a
jrvanwhy <jrvanwhy@a504bc44-2baa-4cb8-4136-ba805e94d278>
2012-08-01 20:28:21 +0000

    More RT Ops work.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@1708 a504bc44-2baa-4cb8-4136-ba805e94d278

--- ethercat/ecat_test/main.cpp
+++ ethercat/ecat_test/main.cpp
@@ -19,18 +19,31 @@ int main(int argc, char ** argv) {
 	}
 	
 	ec_slave_config_t* sc0 = ecrt_master_slave_config(ec_master, 0, 0, VENDOR_ID, PRODUCT_CODE);
+	//ec_slave_config_t* sc1 = ecrt_master_slave_config(ec_master, 0, 1, VENDOR_ID, PRODUCT_CODE);
 	
-	if (ecrt_slave_config_pdos(sc0, 4, slave_0_syncs)) {
+	if (ecrt_slave_config_pdos(sc0, 4, slave_0_syncs)/* || ecrt_slave_config_pdos(sc1, 4, slave_1_syncs)*/) {
 		perror("ecrt_slave_config_pdos() failed.");
 		ecrt_release_master(ec_master);
 		return 3;
 	}
 	
-	unsigned int command_off;
-	unsigned int counter_off;
+	unsigned int command_off0;
+	unsigned int current_off0;
+	unsigned int timestep_off0;
+	unsigned int encoder_off0;
+	/*unsigned int command_off1;
+	unsigned int current_off1;
+	unsigned int timestep_off1;
+	unsigned int encoder_off1;*/
 	ec_pdo_entry_reg_t entry_regs[] = { 
-		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x5, 0x1,  &command_off, NULL},
-		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x6, 0x1,  &counter_off, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x5, 0x1,  &command_off0, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x3, 0x2,  &current_off0, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x6, 0x1, &timestep_off0, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x7, 0x2,  &encoder_off0, NULL},
+		/*{0, 1, VENDOR_ID, PRODUCT_CODE, 0x5, 0x1,  &command_off1, NULL},
+		{0, 1, VENDOR_ID, PRODUCT_CODE, 0x3, 0x2,  &current_off1, NULL},
+		{0, 1, VENDOR_ID, PRODUCT_CODE, 0x6, 0x1, &timestep_off1, NULL},
+		{0, 1, VENDOR_ID, PRODUCT_CODE, 0x7, 0x2,  &encoder_off1, NULL},*/
 		{0, 0,      0x00,         0x00, 0x0, 0x0,           NULL, NULL}};
 	if (ecrt_domain_reg_pdo_entry_list(domain, entry_regs)) {
 		perror("ecrt_domain_reg_pdo_entry_list() failed");
@@ -45,8 +58,14 @@ int main(int argc, char ** argv) {
 
 	uint8_t* domain_pd = ecrt_domain_data(domain);
 	
-	unsigned char* command  = (unsigned char*) (domain_pd + command_off);
-	uint16_t*      counter  = (uint16_t*)      (domain_pd + counter_off);
+	unsigned char* command0  = (unsigned char*) (domain_pd + command_off0);
+	uint16_t*      current0  = (uint16_t*)      (domain_pd + current_off0);
+	uint16_t*      timestep0 = (uint16_t*)      (domain_pd + timestep_off0);
+	uint32_t*      encoder0  = (uint32_t*)      (domain_pd + encoder_off0);
+	/*unsigned char* command1  = (unsigned char*) (domain_pd + command_off1);
+	uint16_t*      current1  = (uint16_t*)      (domain_pd + current_off1);
+	uint16_t*      timestep1 = (uint16_t*)      (domain_pd + timestep_off1);
+	uint32_t*      encoder1  = (uint32_t*)      (domain_pd + encoder_off1);*/
 	
 	(*command) = 0;
 	char input;
@@ -54,7 +73,7 @@ int main(int argc, char ** argv) {
 	clock_gettime(CLOCK_REALTIME, &cur_time);
 	ecrt_master_application_time(ec_master, EC_NEWTIMEVAL2NANO(cur_time));
 	ecrt_slave_config_dc(sc0, 0x0300, LOOP_PERIOD_NS, LOOP_PERIOD_NS - (cur_time.tv_nsec % LOOP_PERIOD_NS), 0, 0);
-//	ecrt_slave_config_dc(sc1, 0x0300, LOOP_PERIOD_NS, LOOP_PERIOD_NS - (cur_time.tv_nsec % LOOP_PERIOD_NS), 0, 0);
+	//ecrt_slave_config_dc(sc1, 0x0300, LOOP_PERIOD_NS, LOOP_PERIOD_NS - (cur_time.tv_nsec % LOOP_PERIOD_NS), 0, 0);
 	
 	long tgt_time = cur_time.tv_nsec;
 	timespec wait_time = {
@@ -70,7 +89,7 @@ int main(int argc, char ** argv) {
 //		if (*counter < 20000)
 		*counter += 1;
 		printf("command: %d\n",*command);
-		//printf("command: %3u, timestep: %3u, encoder: %8u\n", (unsigned int) *command, *timestep, (unsigned int) *encoder);
+		printf("command0: %3u, timestep0: %3u, encoder0: %8u\n", (unsigned int) *command0, *timestep0, (unsigned int) *encoder0);
 		
 		clock_gettime(CLOCK_REALTIME, &cur_time);
 		ecrt_master_application_time(ec_master, EC_NEWTIMEVAL2NANO(cur_time));
@@ -78,7 +97,7 @@ int main(int argc, char ** argv) {
 		ecrt_master_sync_slave_clocks(ec_master);
 		ecrt_domain_queue(domain);
 		ecrt_master_send(ec_master);
-		usleep(10);
+		usleep(300);
 		ecrt_master_receive(ec_master);
 		ecrt_domain_process(domain);
 		wait_time.tv_nsec = LOOP_PERIOD_NS - (cur_time.tv_nsec % LOOP_PERIOD_NS);
