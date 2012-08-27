dc05784c967ac5fc78620738c9b857afc41cdcd1
yoos <yoos117@gmail.com>
2012-08-27 21:43:40 +0000

    Fix typo.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@2193 a504bc44-2baa-4cb8-4136-ba805e94d278

--- software/atrias/templates/atc_component/include/atc_component/controller_gui.h
+++ software/atrias/templates/atc_component/include/atc_component/controller_gui.h
@@ -31,7 +31,7 @@ Gtk::CheckButton *set_position_checkbutton;
 
 // Parameters
 //     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
-//     arise when both C and python nodes access the same paarmeter. See:
+//     arise when both C and python nodes access the same parameter. See:
 //     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
 double torque_A_param;
 double torque_B_param;
