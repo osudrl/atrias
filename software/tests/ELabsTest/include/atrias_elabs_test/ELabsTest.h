6d8def59cff971516f2597458e9e559e68cf302d
jrvanwhy@gmail.com <jrvanwhy@gmail.com@a504bc44-2baa-4cb8-4136-ba805e94d278>
2012-08-27 20:34:43 +0000

    Turns out that EtherLabs causes a switch to secondary mode in Xenomai. Test complete.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@2185 a504bc44-2baa-4cb8-4136-ba805e94d278

--- software/tests/ELabsTest/include/atrias_elabs_test/ELabsTest.h
+++ software/tests/ELabsTest/include/atrias_elabs_test/ELabsTest.h
@@ -1,2 +1,54 @@
-#include <ecrt.h>
+#ifndef ELABSTEST_H
+#define ELABSTEST_H
 
+#include <ecrt.h>
+#include <rtt/TaskContext.hpp>
+#include <rtt/Component.hpp>
+#include <rtt/Logger.hpp>
+#include <time.h>
+
+#include <robot_invariant_defs.h>
+
+#define VENDOR_ID MEDULLA_VENDOR_ID
+#define PRODUCT_CODE MEDULLA_TEST_PRODUCT_CODE
+
+/* Master 0, Slave 0, "ATRIAS 2.1 EtherCAT Test (Medulla 1.5)"
+ * Vendor ID:       0x0000060f
+ * Product code:    0x00000004
+ * Revision number: 0x00000003
+ */
+
+ec_pdo_entry_info_t slave_0_pdo_entries[] = {
+    {0x0005, 0x01, 8}, /* Command */
+    {0x0003, 0x02, 16}, /* Motor Current */
+    {0x0006, 0x01, 16}, /* Timestep */
+    {0x0007, 0x02, 32}, /* Encoder 0 */
+};
+
+ec_pdo_info_t slave_0_pdos[] = {
+    {0x1600, 2, slave_0_pdo_entries + 0}, /* uControllerInput */
+    {0x1a00, 2, slave_0_pdo_entries + 2}, /* uControllerOutput */
+};
+
+ec_sync_info_t slave_0_syncs[] = {
+    {0, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE},
+    {1, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE},
+    {2, EC_DIR_OUTPUT, 1, slave_0_pdos + 0, EC_WD_DISABLE},
+    {3, EC_DIR_INPUT, 1, slave_0_pdos + 1, EC_WD_DISABLE},
+    {0xff}
+};
+
+using namespace RTT;
+
+class ELabsTest : public TaskContext {
+	ec_master_t* ec_master;
+	ec_domain_t* domain;
+	public:
+		ELabsTest(std::string name);
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void cleanupHook();
+};
+
+#endif // ELABSTEST_H
--- software/tests/ELabsTest/src/ELabsTest.cpp
+++ software/tests/ELabsTest/src/ELabsTest.cpp
@@ -1,1 +1,80 @@
 #include "atrias_elabs_test/ELabsTest.h"
+
+ELabsTest::ELabsTest(std::string name) : TaskContext(name) {
+	return;
+}
+
+bool ELabsTest::configureHook() {
+	ec_master = ecrt_request_master(0);
+	if (!ec_master) {
+		log(Error) << "ecrt_request_master() returned NULL!" << endlog();
+		return false;
+	}
+	
+	domain = ecrt_master_create_domain (ec_master);
+	if (!domain) {
+		log(Error) << "ecrt_master_create_domain() returned NULL!" << endlog();
+		ecrt_release_master(ec_master);
+		return false;
+	}
+	
+	ec_slave_config_t* sc0 = ecrt_master_slave_config(ec_master, 0, 0, VENDOR_ID, PRODUCT_CODE);
+	if (!sc0) {
+		log(Error) << "ecrt_master_slave_config returned NULL!" << endlog();
+		ecrt_release_master(ec_master);
+		return false;
+	}
+	
+	if (ecrt_slave_config_pdos(sc0, 4, slave_0_syncs)) {
+		log(Error) << "ecrt_slave_config_pdos failed!" << endlog();
+		ecrt_release_master(ec_master);
+		return false;
+	}
+	
+	unsigned int command_off;
+	unsigned int current_off;
+	unsigned int timestep_off;
+	unsigned int encoder_off;
+	ec_pdo_entry_reg_t entry_regs[] = {
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x5, 0x1,  &command_off, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x3, 0x2,  &current_off, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x6, 0x1, &timestep_off, NULL},
+		{0, 0, VENDOR_ID, PRODUCT_CODE, 0x7, 0x2,  &encoder_off, NULL},
+		{0, 0,      0x00,         0x00, 0x0, 0x0,          NULL, NULL}
+	};
+	
+	if (ecrt_domain_reg_pdo_entry_list(domain, entry_regs)) {
+		log(Error) << "ecrt_domain_reg_pdo_entry_list" << endlog();
+		ecrt_release_master(ec_master);
+		return false;
+	}
+	
+	return true;
+}
+
+bool ELabsTest::startHook() {
+	if (ecrt_master_activate(ec_master)) {
+		log(Error) << "ecrt_master_activate() failed!" << endlog();
+		return false;
+	}
+	
+	return true;
+}
+
+void ELabsTest::updateHook() {
+	ecrt_domain_queue(domain);
+	ecrt_master_send(ec_master);
+	timespec delay = {
+		0,
+		300000
+	};
+	nanosleep(&delay, NULL);
+	ecrt_master_receive(ec_master);
+	ecrt_domain_process(domain);
+}
+
+void ELabsTest::cleanupHook() {
+	ecrt_release_master(ec_master);
+}
+
+ORO_CREATE_COMPONENT(ELabsTest)
