06a9dcc134ca306d13ab6ac6ba45a4cbb764fa0b
yoos <yoos117@gmail.com>
2012-08-27 21:32:49 +0000

    Fix torque controller.
    git-svn-id: https://atrias.googlecode.com/svn/trunk@2190 a504bc44-2baa-4cb8-4136-ba805e94d278

--- software/atrias_controllers/atc_motor_torque/include/atc_motor_torque/controller_gui.h
+++ software/atrias_controllers/atc_motor_torque/include/atc_motor_torque/controller_gui.h
@@ -8,21 +8,36 @@
 #ifndef CONTROLLER_GUI_H_
 #define CONTROLLER_GUI_H_
 
-#include <atc_motor_torque/controller_input.h>
+#include <atc_component/controller_input.h>
 #include <atrias_shared/gui_library.h>
 #include <robot_invariant_defs.h>
 #include <ros/ros.h>
 
+// ROS
 ros::NodeHandle nh;
+ros::Subscriber sub;
 ros::Publisher pub;
 
-atc_motor_torque::controller_input controllerDataOut;
+// Data
+atc_component::controller_input controllerDataOut;
+atc_component::controller_status controllerDataIn;
 
+// GUI elements
 Gtk::HScale *torque_A_hscale,
         *torque_B_hscale,
         *torque_hip_hscale;
 
 Gtk::CheckButton *set_position_checkbutton;
 
+// Parameters
+//     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
+//     arise when both C and python nodes access the same paarmeter. See:
+//     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
+double torque_A_param;
+double torque_B_param;
+double torque_hip_param;
+
+void controllerCallback(const atc_component::controller_status &status);
 
 #endif /* CONTROLLER_GUI_H_ */
+
--- software/atrias_controllers/atc_motor_torque/src/controller_gui.cpp
+++ software/atrias_controllers/atc_motor_torque/src/controller_gui.cpp
@@ -22,16 +22,42 @@ bool guiInit(Glib::RefPtr<Gtk::Builder> gui) {
         torque_B_hscale->set_range(-10., 10.);
         torque_hip_hscale->set_range(-10., 10.);
 
+        // Set up subscriber and publisher.
+        sub = nh.subscribe("atc_component_status", 0, controllerCallback);
         pub = nh.advertise<atc_motor_torque::controller_input>("atc_motor_torque_input", 0);
         return true;
     }
     return false;
 }
 
+void controllerCallback(const atc_component::controller_status &status) {
+    controllerDataIn = status;
+}
+
+//! \brief Get parameters from the server and configure GUI accordingly.
+void getParameters() {
+    // Get parameters in the atrias_gui namespace.
+    nh.getParam("/atrias_gui/torque_A", torque_A_param);
+    nh.getParam("/atrias_gui/torque_B", torque_B_param);
+    nh.getParam("/atrias_gui/torque_hip", torque_hip_param);
+
+    // Configure the GUI.
+    torque_A_hscale->set_value(torque_A_param);
+    torque_B_hscale->set_value(torque_B_param);
+    torque_hip_hscale->set_value(torque_hip_param);
+}
+
+//! \brief Set parameters on server according to current GUI settings.
+void setParameters() {
+    nh.setParam("/atrias_gui/torque_A", torque_A_param);
+    nh.setParam("/atrias_gui/torque_B", torque_B_param);
+    nh.setParam("/atrias_gui/torque_hip", torque_hip_param);
+}
+
 void guiUpdate() {
-    controllerDataOut.des_motor_torque_A = torque_A_hscale->get_value();
-    controllerDataOut.des_motor_torque_B = torque_B_hscale->get_value();
-    controllerDataOut.des_motor_torque_hip = torque_hip_hscale->get_value();
+    controllerDataOut.des_motor_torque_A   = torque_A_param   = torque_A_hscale->get_value();
+    controllerDataOut.des_motor_torque_B   = torque_B_param   = torque_B_hscale->get_value();
+    controllerDataOut.des_motor_torque_hip = torque_hip_param = torque_hip_hscale->get_value();
     pub.publish(controllerDataOut);
 }
 
