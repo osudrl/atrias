4bbd288761e41bc6b41d22ff8f6581d370d78d41
Andrew Peekema <apeekema@gmail.com>
2012-08-29 11:27:39 -0700

    Creating the biped simulation

--- software/atrias_sim/src/freeze_pose.cpp
+++ software/atrias_sim/src/freeze_pose.cpp
@@ -9,47 +9,47 @@ GZ_REGISTER_WORLD_PLUGIN(FreezePose)
 
 FreezePose::FreezePose()
 {
-	KP = 100000;
-	KD = 100;
+    KP = 100000;
+    KD = 100;
 }
 
 void FreezePose::Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
 {
-	// Pointer to the world
-	this->world = _parent;
+    // Pointer to the world
+    this->world = _parent;
 
-	// Pointer to the model
-	if (_sdf->HasElement("modelName"))
-	{
-		this->modelName = _sdf->GetElement("modelName")->GetValueString();
-		this->model = this->world->GetModel(this->modelName);
-	}
-	else
-		gzerr << "Gazebo freezePose plugin missing valid modelName\n";
+    // Pointer to the model
+    if (_sdf->HasElement("modelName"))
+    {
+        this->modelName = _sdf->GetElement("modelName")->GetValueString();
+        this->model = this->world->GetModel(this->modelName);
+    }
+    else
+        gzerr << "Gazebo freezePose plugin missing valid modelName\n";
 
-	// Pointer to the link
-	if (_sdf->HasElement("linkName"))
-	{
-		this->linkName = _sdf->GetElement("linkName")->GetValueString();
-		this->link = this->model->GetLink(this->linkName);
-	}
-	else
-		gzerr << "Gazebo freezePose plugin missing valid linkName\n";
+    // Pointer to the link
+    if (_sdf->HasElement("linkName"))
+    {
+        this->linkName = _sdf->GetElement("linkName")->GetValueString();
+        this->link = this->model->GetLink(this->linkName);
+    }
+    else
+        gzerr << "Gazebo freezePose plugin missing valid linkName\n";
 
-	// Listen to the update event. This event is broadcast every
-	// simulation iteration.
-	this->updateConnection = event::Events::ConnectWorldUpdateStart(
-		boost::bind(&FreezePose::OnUpdate, this));
+    // Listen to the update event. This event is broadcast every
+    // simulation iteration.
+    this->updateConnection = event::Events::ConnectWorldUpdateStart(
+        boost::bind(&FreezePose::OnUpdate, this));
 
-	desired_pose = this->link->GetWorldPose();
+    desired_pose = this->link->GetWorldPose();
 }
 
 
 // Called by the world update start event
 void FreezePose::OnUpdate()
 {
-	force = (this->desired_pose.pos - this->link->GetWorldPose().pos) * KP - this->link->GetRelativeLinearVel() * KD;
-	this->link->SetForce(force);
+    force = (this->desired_pose.pos - this->link->GetWorldPose().pos) * KP - this->link->GetRelativeLinearVel() * KD;
+    this->link->SetForce(force);
 
-	this->link->SetTorque((this->desired_pose.rot.GetAsEuler() - this->link->GetWorldPose().rot.GetAsEuler()) * KP - this->link->GetRelativeAngularVel() * KD);
+    this->link->SetTorque((this->desired_pose.rot.GetAsEuler() - this->link->GetWorldPose().rot.GetAsEuler()) * KP - this->link->GetRelativeAngularVel() * KD);
 }
