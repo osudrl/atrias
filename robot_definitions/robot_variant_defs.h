e3db2b22a31ed22cdfbc7626a5ce82a8c5a77057
Michael Anderson <broodoobob@gmail.com>
2012-08-30 14:43:06 -0700

    Updating calibration values for leg #1

--- robot_definitions/robot_variant_defs.h
+++ robot_definitions/robot_variant_defs.h
@@ -1,33 +1,33 @@
 #ifndef ROBOT_VARIANT_DEFS_H
 #define ROBOT_VARIANT_DEFS_H
 
-// This is for Dr. Grizzle's robot.
+// This is for biped #1
 
 #define NUM_MEDULLAS_ON_ROBOT    7
 
 // These have been set
-#define LEFT_LEG_A_CALIB_VAL     262226369LL
-#define LEFT_LEG_B_CALIB_VAL     261535112LL
+#define LEFT_LEG_A_CALIB_VAL     263651250LL
+#define LEFT_LEG_B_CALIB_VAL     262942179LL
 // These have not been set yet.
 #define RIGHT_LEG_A_CALIB_VAL    352351997LL
 #define RIGHT_LEG_B_CALIB_VAL    348884314LL
 
 // These have not been set yet
 #define LEFT_LEG_A_RAD_PER_CNT    9.8039216e-09
-#define LEFT_LEG_B_RAD_PER_CNT   -9.8039216e-09
+#define LEFT_LEG_B_RAD_PER_CNT    -9.8039216e-09
 #define RIGHT_LEG_A_RAD_PER_CNT   9.8039216e-09
 #define RIGHT_LEG_B_RAD_PER_CNT   9.8039216e-09
 
 // These have been set.
-#define LEFT_TRAN_A_CALIB_VAL    196050449LL
-#define LEFT_TRAN_B_CALIB_VAL    198113672LL
+#define LEFT_TRAN_A_CALIB_VAL    199387520LL
+#define LEFT_TRAN_B_CALIB_VAL    197437370LL
 // These have not been set yet.
 #define RIGHT_TRAN_A_CALIB_VAL   284287500
 #define RIGHT_TRAN_B_CALIB_VAL   285247647
 
 // These have not been set yet
 #define LEFT_TRAN_A_RAD_PER_CNT   9.8039216e-09
-#define LEFT_TRAN_B_RAD_PER_CNT  -9.8039216e-09
+#define LEFT_TRAN_B_RAD_PER_CNT   -9.8039216e-09
 #define RIGHT_TRAN_A_RAD_PER_CNT  9.8039216e-09
 #define RIGHT_TRAN_B_RAD_PER_CNT  9.8039216e-09
 
