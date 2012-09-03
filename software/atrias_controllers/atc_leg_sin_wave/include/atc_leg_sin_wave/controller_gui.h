395617f0a9cbfd05e3c3a8b5daf2114c88d1ebfc
yoos <yoos117@gmail.com>
2012-09-02 22:36:01 -0700

    Set up parameters for sine wave controller GUI.

--- software/atrias_controllers/atc_leg_sin_wave/include/atc_leg_sin_wave/controller_gui.h
+++ software/atrias_controllers/atc_leg_sin_wave/include/atc_leg_sin_wave/controller_gui.h
@@ -26,4 +26,17 @@ Gtk::HScale *leg_angle_amplitude_hscale,
             *p_sine_wave_hscale,
             *d_sine_wave_hscale;
 
+// Parameters
+//     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
+//     arise when both C and python nodes access the same parameter. See:
+//     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
+double leg_angle_amplitude_param;
+double leg_angle_frequency_param;
+double leg_length_amplitude_param;
+double leg_length_frequency_param;
+double p_gain_param;
+double d_gain_param;
+
+void controllerCallback(const atc_component::controller_status &status);
+
 #endif /* CONTROLLER_GUI_H_ */
