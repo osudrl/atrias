5b2bf0bd4d39141002865c677df541d53e6790ca
root <root@drl-guilaptop.(none)>
2012-09-12 18:07:02 -0700

    Progress.

--- software/atrias/scripts/log_data.py
+++ software/atrias/scripts/log_data.py
@@ -4,31 +4,37 @@ import roslib; roslib.load_manifest("atrias_msgs")
 import rospy
 from signal import signal, SIGINT
 from atrias_msgs.msg import log_request
-from os import system, fork, getpid
+import os
+import subprocess
+import sys
 
 currentlyLogging = False
 
 def logRequestCallback (req):
     global currentlyLogging
     if req.enableLogging and not currentlyLogging:
-        child_pid = fork()
+        child_pid = os.fork()
         if child_pid == 0:
-            print "Child process: PID# %s" % getpid()
-            system("roslaunch atrias rosbag.launch")
+            print "Child process: PID# %s" % os.getpid()
+            subprocess.call(["roslaunch", "atrias", "rosbag.launch"])
+            sys.exit()
         else:
-            print "Parent process: PID# %s" % getpid()
+            print "Parent process: PID# %s" % os.getpid()
             print "Launched rosbag."
+            os.waitpid(-1, os.WNOHANG)   # Reap children.
             currentlyLogging = True
             print "Currently logging."
 
     elif not req.enableLogging and currentlyLogging:
-        child_pid = fork()
+        child_pid = os.fork()
         if child_pid == 0:
-            print "Child process: PID# %s" % getpid()
-            system("rosnode kill atrias_rosbag")
+            print "Child process: PID# %s" % os.getpid()
+            subprocess.call(["rosnode", "kill", "atrias_rosbag"])
+            sys.exit()
         else:
-            print "Parent process: PID# %s" % getpid()
+            print "Parent process: PID# %s" % os.getpid()
             print "Tried to kill rosbag."
+            os.wait()
             currentlyLogging = False
             print "I don't think I'm logging anymore."
 
