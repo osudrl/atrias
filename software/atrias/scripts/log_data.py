5ae9d94577f69cd62cdbda6af5917e23cae6251d
yoos <yoos117@gmail.com>
2012-09-12 22:38:13 -0700

    Simplify rosnode kill invocation.

--- software/atrias/scripts/log_data.py
+++ software/atrias/scripts/log_data.py
@@ -14,13 +14,14 @@ def logRequestCallback (req):
     global roslaunchProcess, currentlyLogging
 
     if req.enableLogging and not currentlyLogging:
+        # Spawn off roslaunch process (non-blocking).
         roslaunchProcess = subprocess.Popen(["roslaunch", "atrias", "rosbag.launch"])
         currentlyLogging = True
 
     elif not req.enableLogging and currentlyLogging:
-        killProcess = subprocess.Popen(["rosnode", "kill", "atrias_rosbag"])
+        # Invoke rosnode kill and wait for it to return.
+        subprocess.call(["rosnode", "kill", "atrias_rosbag"])
         roslaunchProcess.wait()   # Wait for processes to die, and reap them.
-        killProcess.wait()
         currentlyLogging = False
 
 
