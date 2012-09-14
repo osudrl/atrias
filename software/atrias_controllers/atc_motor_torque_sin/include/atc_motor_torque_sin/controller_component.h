f53f28181056e69293f9b888a3c7705916c81506
Andrew Peekema <apeekema@gmail.com>
2012-09-14 13:48:38 -0700

    Blank slate for the atc_motor_torque_sin controller

---
+++ software/atrias_controllers/atc_motor_torque_sin/include/atc_motor_torque_sin/controller_component.h
@@ -0,0 +1,99 @@
+/*! \file controller_component.h
+ *  \author Andrew Peekema
+ *  \brief Orocos Component header for the atc_motor_torque_sin controller.
+ */
+
+// Orocos 
+#include <rtt/os/main.h>
+#include <rtt/RTT.hpp>
+#include <rtt/Logger.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/OperationCaller.hpp>
+#include <rtt/Component.hpp>
+#include <rtt/Property.hpp>
+
+// C
+#include <stdlib.h>
+
+#include <atrias_shared/GuiPublishTimer.h>
+#include <atrias_shared/globals.h>
+#include <robot_invariant_defs.h>
+
+// Datatypes
+#include <atrias_msgs/robot_state.h>
+#include <atrias_msgs/controller_output.h>
+#include <atc_motor_torque_sin/controller_input.h>
+#include <atrias_shared/controller_structs.h>
+
+using namespace RTT;
+using namespace Orocos;
+using namespace atc_motor_torque_sin;
+
+namespace atrias {
+using namespace shared;
+namespace controller {
+
+class ATCMotorTorqueSin : public TaskContext {
+private:
+    atrias_msgs::robot_state        robotState;
+    atrias_msgs::controller_output  controllerOutput;
+
+    controller_input                guiIn;
+
+    InputPort<controller_input>     guiDataIn;
+
+    // This Operation is called by the RT Operations Manager.
+    atrias_msgs::controller_output runController(atrias_msgs::robot_state);
+
+    // Variables for subcontrollers
+    std::string pd0Name;
+    std::string pd1Name;
+    std::string pd2Name;
+    std::string pd3Name;
+    std::string sin0Name;
+    std::string sin1Name;
+
+    TaskContext *pd0;
+    TaskContext *pd1;
+    TaskContext *pd2;
+    TaskContext *pd3;
+    TaskContext *sin0;
+    TaskContext *sin1;
+
+    Property<double> P0;
+    Property<double> D0;
+    Property<double> P1;
+    Property<double> D1;
+    Property<double> P2;
+    Property<double> D2;
+    Property<double> P3;
+    Property<double> D3;
+
+    OperationCaller<SinOut(double, double)> sin0Controller;
+    OperationCaller<SinOut(double, double)> sin1Controller;
+    OperationCaller<double(double, double, double, double)> pd0Controller;
+    OperationCaller<double(double, double, double, double)> pd1Controller;
+    OperationCaller<double(double, double, double, double)> pd2Controller;
+    OperationCaller<double(double, double, double, double)> pd3Controller;
+
+    // Math variables
+    double centerBAngle;
+    double centerAAngle;
+    double targetPos, currentPos, targetVel, currentVel;
+    SinOut leftMotorBSin;
+    SinOut rightMotorBSin;
+
+
+public:
+    // Constructor
+    ATCMotorTorqueSin(std::string name);
+
+    // Standard Orocos hooks
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+};
+}
+}
---
+++ software/atrias_controllers/atc_motor_torque_sin/src/controller_component.cpp
@@ -0,0 +1,185 @@
+/*! \file controller_component.cpp
+ *  \author Andrew Peekema
+ *  \brief Orocos Component code for the motor torque sin controller
+ */
+
+#include <atc_motor_torque_sin/controller_component.h>
+
+namespace atrias {
+namespace controller {
+
+ATCMotorTorqueSin::ATCMotorTorqueSin(std::string name):
+    RTT::TaskContext(name),
+    guiDataIn("gui_data_in")
+{
+    this->provides("atc")
+        ->addOperation("runController", &ATCMotorTorqueSin::runController, this, OwnThread)
+        .doc("Get robot_state from RTOps and return controller output.");
+
+    // Add properties.
+    this->addProperty("pd0Name", pd0Name)
+        .doc("Name of 0th PD subcontroller.");
+    this->addProperty("pd1Name", pd1Name)
+        .doc("Name of 1st PD subcontroller.");
+    this->addProperty("pd2Name", pd2Name)
+        .doc("Name of 2st PD subcontroller.");
+    this->addProperty("pd3Name", pd3Name)
+        .doc("Name of 3st PD subcontroller.");
+    this->addProperty("sin0Name", sin0Name)
+        .doc("Name of 0th sin generator subcontroller.");
+    this->addProperty("sin1Name", sin1Name)
+        .doc("Name of 1st sin generator subcontroller.");
+
+    // Add ports.
+    addEventPort(guiDataIn);
+
+    // Set Defaults
+    targetPos = 0;
+    currentPos = 0;
+    targetVel = 0;
+    currentVel = 0;
+
+    log(Info) << "[ATCMTS] Motor sin wave controller constructed!" << endlog();
+}
+
+// Put control code here.
+atrias_msgs::controller_output ATCMotorTorqueSin::runController(atrias_msgs::robot_state rs) {
+    // Only run the controller when we're enabled
+    if ((uint8_t)rs.cmState != (uint8_t)controllerManager::RtOpsCommand::ENABLE)
+    {
+        // Do nothing
+        controllerOutput.lLeg.motorCurrentA = 0.0;
+        controllerOutput.lLeg.motorCurrentB = 0.0;
+        controllerOutput.lLeg.motorCurrentHip = 0.0;
+        controllerOutput.rLeg.motorCurrentA = 0.0;
+        controllerOutput.rLeg.motorCurrentB = 0.0;
+        controllerOutput.rLeg.motorCurrentHip = 0.0;
+        return controllerOutput;
+    }
+
+    // Get a sinusoidal input
+    leftMotorBSin = sin0Controller(guiIn.leg_ang_frq, guiIn.leg_ang_amp);
+    rightMotorBSin = sin1Controller(guiIn.leg_ang_frq, guiIn.leg_ang_amp);
+    // motorBSin.pos / .vel
+
+    // Set resonable center positions
+    centerAAngle = M_PI/4;
+    centerBAngle = 3*M_PI/4;
+    leftMotorBSin.pos = leftMotorBSin.pos + centerBAngle;
+    rightMotorBSin.pos = rightMotorBSin.pos + centerBAngle;
+
+    // Set the PD gains
+    // Left leg
+    // motorA
+    P0.set(guiIn.p_gain);
+    D0.set(guiIn.d_gain);
+    // motorB
+    P1.set(guiIn.p_gain);
+    D1.set(guiIn.d_gain);
+    // Right leg
+    // motorA
+    P2.set(guiIn.p_gain);
+    D2.set(guiIn.d_gain);
+    // motorB
+    P3.set(guiIn.p_gain);
+    D3.set(guiIn.d_gain);
+
+    // Left Leg
+    // Calculate motorA current
+    targetPos = centerAAngle;
+    currentPos = rs.lLeg.halfA.motorAngle;
+    targetVel = 0.0;
+    currentVel = rs.lLeg.halfA.motorVelocity;
+    controllerOutput.lLeg.motorCurrentA = pd0Controller(targetPos, currentPos, targetVel, currentVel);
+    
+    // Calculate motorB current
+    targetPos = leftMotorBSin.pos;
+    currentPos = rs.lLeg.halfB.motorAngle;
+    targetVel = leftMotorBSin.vel;
+    currentVel = rs.lLeg.halfB.motorVelocity;
+    controllerOutput.lLeg.motorCurrentB = pd1Controller(targetPos, currentPos, targetVel, currentVel);
+
+    // Right Leg
+    // Calculate motorA current
+    targetPos = centerAAngle;
+    currentPos = rs.rLeg.halfA.motorAngle;
+    targetVel = 0.0;
+    currentVel = rs.rLeg.halfA.motorVelocity;
+    controllerOutput.rLeg.motorCurrentA = pd2Controller(targetPos, currentPos, targetVel, currentVel);
+    
+    // Calculate motorB current
+    targetPos = rightMotorBSin.pos;
+    currentPos = rs.rLeg.halfB.motorAngle;
+    targetVel = rightMotorBSin.vel;
+    currentVel = rs.rLeg.halfB.motorVelocity;
+    controllerOutput.rLeg.motorCurrentB = pd3Controller(targetPos, currentPos, targetVel, currentVel);
+
+    controllerOutput.command = medulla_state_run;
+
+    return controllerOutput;
+}
+
+// Don't put control code below here!
+bool ATCMotorTorqueSin::configureHook() {
+    // Connect to the subcontrollers
+    // Sin controllers
+    sin0 = this->getPeer(sin0Name);
+    if (sin0)
+        sin0Controller = sin0->provides("sg")->getOperation("runController");
+    sin1 = this->getPeer(sin1Name);
+    if (sin1)
+        sin1Controller = sin1->provides("sg")->getOperation("runController");
+
+    // PD controllers
+    pd0 = this->getPeer(pd0Name);
+    if (pd0)
+        pd0Controller = pd0->provides("pd")->getOperation("runController");
+
+    pd1 = this->getPeer(pd1Name);
+    if (pd1)
+        pd1Controller = pd1->provides("pd")->getOperation("runController");
+
+    pd2 = this->getPeer(pd2Name);
+    if (pd2)
+        pd2Controller = pd2->provides("pd")->getOperation("runController");
+
+    pd3 = this->getPeer(pd3Name);
+    if (pd3)
+        pd3Controller = pd3->provides("pd")->getOperation("runController");
+
+    // Get references to the attributes
+    P0 = pd0->properties()->getProperty("P");
+    D0 = pd0->properties()->getProperty("D");
+    P1 = pd1->properties()->getProperty("P");
+    D1 = pd1->properties()->getProperty("D");
+    P2 = pd2->properties()->getProperty("P");
+    D2 = pd2->properties()->getProperty("D");
+    P3 = pd3->properties()->getProperty("P");
+    D3 = pd3->properties()->getProperty("D");
+
+    log(Info) << "[ATCMTS] configured!" << endlog();
+    return true;
+}
+
+bool ATCMotorTorqueSin::startHook() {
+    log(Info) << "[ATCMTS] started!" << endlog();
+    return true;
+}
+
+void ATCMotorTorqueSin::updateHook() {
+    guiDataIn.read(guiIn);
+}
+
+void ATCMotorTorqueSin::stopHook() {
+    log(Info) << "[ATCMTS] stopped!" << endlog();
+}
+
+void ATCMotorTorqueSin::cleanupHook() {
+    log(Info) << "[ATCMTS] cleaned up!" << endlog();
+}
+
+ORO_CREATE_COMPONENT(ATCMotorTorqueSin)
+
+}
+}
+
