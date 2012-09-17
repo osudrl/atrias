53f889f2620cdc0a2eb3a084673039b577ce2709
yoos <yoos117@gmail.com>
2012-09-17 14:21:23 -0700

    Update GUI.

--- software/atrias_controllers/atc_umich_1/src/controller_gui.cpp
+++ software/atrias_controllers/atc_umich_1/src/controller_gui.cpp
@@ -17,9 +17,19 @@ bool guiInit(Glib::RefPtr<Gtk::Builder> gui) {
     gui->get_widget("q1l_hscale", q1l_hscale);
     gui->get_widget("q2l_hscale", q2l_hscale);
     gui->get_widget("q3l_hscale", q3l_hscale);
+    gui->get_widget("kp1_hscale", kp1_hscale);
+    gui->get_widget("kp2_hscale", kp2_hscale);
+    gui->get_widget("kp3_hscale", kp3_hscale);
+    gui->get_widget("kd1_hscale", kd1_hscale);
+    gui->get_widget("kd2_hscale", kd2_hscale);
+    gui->get_widget("kd3_hscale", kd3_hscale);
+    gui->get_widget("epsilon_spinbutton", epsilon_spinbutton);
 
     if (q1r_hscale && q2r_hscale && q3r_hscale &&
-	q1l_hscale && q2l_hscale && q3l_hscale) {
+        q1l_hscale && q2l_hscale && q3l_hscale &&
+        kp1_hscale && kp2_hscale && kp3_hscale &&
+        kd1_hscale && kd2_hscale && kd3_hscale &&
+        epsilon_spinbutton) {
         // Set ranges.
         q1r_hscale->set_range(140, 220);
         q2r_hscale->set_range(140, 220);
@@ -27,6 +37,13 @@ bool guiInit(Glib::RefPtr<Gtk::Builder> gui) {
         q1l_hscale->set_range(140, 220);
         q2l_hscale->set_range(140, 220);
         q3l_hscale->set_range(-15, 15);
+        kp1_hscale->set_range(   ,    );
+        kp2_hscale->set_range(   ,    );
+        kp3_hscale->set_range(   ,    );
+        kd1_hscale->set_range(   ,    );
+        kd2_hscale->set_range(   ,    );
+        kd3_hscale->set_range(   ,    );
+        epsilon_spinbutton->set_range(,);
 
         // Set up subscriber and publisher.
         pub = nh.advertise<atc_umich_1::controller_input>("atc_umich_1_input", 0);
@@ -44,6 +61,13 @@ void getParameters() {
     nh.getParam("/atrias_gui/q1l", controllerDataOut.q1l);
     nh.getParam("/atrias_gui/q2l", controllerDataOut.q2l);
     nh.getParam("/atrias_gui/q3l", controllerDataOut.q3l);
+    nh.getParam("/atrias_gui/kp1", controllerDataOut.kp1);
+    nh.getParam("/atrias_gui/kp2", controllerDataOut.kp2);
+    nh.getParam("/atrias_gui/kp3", controllerDataOut.kp3);
+    nh.getParam("/atrias_gui/kd1", controllerDataOut.kd1);
+    nh.getParam("/atrias_gui/kd2", controllerDataOut.kd2);
+    nh.getParam("/atrias_gui/kd3", controllerDataOut.kd3);
+    nh.getParam("/atrias_gui/epsilon", controllerDataOut.epsilon);
 
     // Configure the GUI.
     q1r_hscale->set_value(controllerDataOut.q1r);
@@ -52,6 +76,13 @@ void getParameters() {
     q1l_hscale->set_value(controllerDataOut.q1l);
     q2l_hscale->set_value(controllerDataOut.q2l);
     q3l_hscale->set_value(controllerDataOut.q3l);
+    q3l_hscale->set_value(controllerDataOut.kp1);
+    q3l_hscale->set_value(controllerDataOut.kp2);
+    q3l_hscale->set_value(controllerDataOut.kp3);
+    q3l_hscale->set_value(controllerDataOut.kd1);
+    q3l_hscale->set_value(controllerDataOut.kd2);
+    q3l_hscale->set_value(controllerDataOut.kd3);
+    epsilon_spinbutton->set_value(controllerDataOut.epsilon);
 }
 
 //! \brief Set parameters on server according to current GUI settings.
@@ -62,6 +93,13 @@ void setParameters() {
     nh.setParam("/atrias_gui/q1l", controllerDataOut.q1l);
     nh.setParam("/atrias_gui/q2l", controllerDataOut.q2l);
     nh.setParam("/atrias_gui/q3l", controllerDataOut.q3l);
+    nh.setParam("/atrias_gui/kp1", controllerDataOut.kp1);
+    nh.setParam("/atrias_gui/kp2", controllerDataOut.kp2);
+    nh.setParam("/atrias_gui/kp3", controllerDataOut.kp3);
+    nh.setParam("/atrias_gui/kd1", controllerDataOut.kd1);
+    nh.setParam("/atrias_gui/kd2", controllerDataOut.kd2);
+    nh.setParam("/atrias_gui/kd3", controllerDataOut.kd3);
+    nh.setParam("/atrias_gui/epsilon", controllerDataOut.epsilon);
 }
 
 //! \brief Update the GUI.
@@ -72,6 +110,13 @@ void guiUpdate() {
     controllerDataOut.q1l = q1l_hscale->get_value();
     controllerDataOut.q2l = q2l_hscale->get_value();
     controllerDataOut.q3l = q3l_hscale->get_value();
+    controllerDataOut.kp1 = kp1_hscale->get_value();
+    controllerDataOut.kp2 = kp2_hscale->get_value();
+    controllerDataOut.kp3 = kp3_hscale->get_value();
+    controllerDataOut.kd1 = kd1_hscale->get_value();
+    controllerDataOut.kd2 = kd2_hscale->get_value();
+    controllerDataOut.kd3 = kd3_hscale->get_value();
+    controllerDataOut.epsilon = epsilon_spinbutton->get_value();
     pub.publish(controllerDataOut);
 }
 
