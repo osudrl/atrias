a935fc6b0a7be46489eaa8ef2ce3a8a227e9914c
Andrew Peekema <apeekema@gmail.com>
2012-09-18 13:15:16 -0700

    Tweaked the build_simulink_controllers

--- software/atrias/scripts/build_simulink_controllers.sh
+++ software/atrias/scripts/build_simulink_controllers.sh
@@ -1,5 +1,71 @@
 #!/bin/bash
 
+# Define functions
+function chooseAController
+{
+    while [ 1 ]; do
+        echo
+        echo "Simulink controllers:"
+        echo "$controllerList" | sed "s/_ert_shrlib_rtw//" | nl -w1 -s") "
+        echo
+        echo "What controller do you want to add?"
+        read controllerNumber
+        echo
+        echo
+
+        maxNumber=$(echo "$controllerList" | wc -l)
+
+        if [[ $controllerNumber -gt $maxNumber || $controllerNumber -lt 1 ]]; then
+            echo
+            echo "-----------ERROR!-----------"
+            echo " Invalid controller number"
+            echo
+            echo
+        else
+            newController=$(echo "$controllerList" | sed -n ${controllerNumber}p)
+            # If this is a new controller
+            if [ "$(echo ${controllerDirs[@]} | grep $newController)" == "" ]
+            then
+                controllerDirs=( ${controllerDirs[@]} $newController )
+            fi
+            break
+        fi
+    done
+}
+
+function makeMatlabController
+{
+    # The controller directory is the first argument
+    controllerDir="$1"
+    # Convert the makefile to the local machine
+    cd "$controllerDir"
+    controllerMakefile=$(ls | grep mk$ )
+    controllerName="${controllerMakefile%.*}"
+    sed -i "s|^MAKECMD         =.*|MAKECMD         = ${localMatlabPath}/bin/glnxa64/gmake|" "${controllerMakefile}"
+    sed -i "s|^MATLAB_ROOT             =.*|MATLAB_ROOT             = ${localMatlabPath}|" "${controllerMakefile}"
+    sed -i "s|^ALT_MATLAB_ROOT         =.*|ALT_MATLAB_ROOT         = ${localMatlabPath}|" "${controllerMakefile}"
+    sed -i "s|^START_DIR               =.*|START_DIR               = ${simulinkControllersDir}|" "${controllerMakefile}"
+
+    # Clean up previous compiles
+    rm -f *.o
+    rm -f ../$controllerName
+    rm -f ../${controllerName}.so
+    rm -f ../lib${controllerName}.so
+
+    # make the controller
+    make -f ${controllerMakefile}
+
+    # Move the shared library to where ROS expects it
+    cd ..
+    mv "${controllerName}.so" "lib${controllerName}.so"
+
+    # Remove the example main program
+    rm -f $controllerName
+}
+
+
+
+
 # Where is MATLAB located?
 localMatlabPath="/usr/local/MATLAB/R2012a"
 if [ ! -f "${localMatlabPath}/bin/matlab" ]; then
@@ -35,7 +101,6 @@ do
         rm -f "$download"
         rm -f ~/Downloads/$download
         # Feedback
-        echo
         echo "Note:"
         echo "Extracted $download to simulink_controllers"
         echo
@@ -44,50 +109,41 @@ done
 
 # Pick a controller
 controllerList=$(find . -mindepth 1 -maxdepth 1 -type d | sed 's|^./||')
+
 while [ 1 ]; do
-    echo "Simulink controllers:"
-    echo "$controllerList" | sed "s/_ert_shrlib_rtw//" | nl -w1 -s") "
+    clear
+    echo "Controllers to make:"
+    echo "${controllerDirs[@]-None}" | sed "s| |\n|g"
     echo
-    echo "What controller do you want to make?"
-    read controllerNumber
+    echo "What do you want to do?"
+    echo "1) Add a controller to make"
+    echo "2) Add all of the controllers to make"
+    echo "3) Run make"
+    echo "4) Exit"
+    read choice
 
-    maxNumber=$(echo "$controllerList" | wc -l)
-    if [[ "$controllerNumber" > "$maxNumber" || "$controllerNumber" < 1 ]]; then
-        echo
-        echo "ERROR: Invalid controller number"
-        echo
-    else
-        controllerDir=$(echo "$controllerList" | sed -n ${controllerNumber}p)
+    case "$choice" in
+    1)
+        chooseAController
+        ;;
+    2)
+        controllerDirs=( $(find . -mindepth 1 -maxdepth 1 -type d | sed 's|^./||') )
+        clear
+        ;;
+    3)
         break
-    fi
-
+        ;;
+    4)
+        exit
+        ;;
+    *)
+        echo "Invalid input."
+        ;;
+    esac
 done
 
-# Convert the makefile to the local machine
-cd "$controllerDir"
-controllerMakefile=$(ls | grep mk$ )
-controllerName="${controllerMakefile%.*}"
-sed -i "s|^MAKECMD         =.*|MAKECMD         = ${localMatlabPath}/bin/glnxa64/gmake|" "${controllerMakefile}"
-sed -i "s|^MATLAB_ROOT             =.*|MATLAB_ROOT             = ${localMatlabPath}|" "${controllerMakefile}"
-sed -i "s|^ALT_MATLAB_ROOT         =.*|ALT_MATLAB_ROOT         = ${localMatlabPath}|" "${controllerMakefile}"
-sed -i "s|^START_DIR               =.*|START_DIR               = ${simulinkControllersDir}|" "${controllerMakefile}"
 
-# Do we want to make the file?
-echo "Controller makefile converted.  Press enter to make clean and make the controller"
-read
-
-# Clean up previous compiles
-rm -f *.o
-rm -f ../$controllerName
-rm -f ../${controllerName}.so
-rm -f ../lib${controllerName}.so
-
-# make the controller
-make -f ${controllerMakefile}
-
-# Move the shared library to where ROS expects it
-cd ..
-mv "${controllerName}.so" "lib${controllerName}.so"
-
-# Remove the example main program
-rm -f $controllerName
+for controllerDir in ${controllerDirs[@]}
+do
+    makeMatlabController $controllerDir
+done
