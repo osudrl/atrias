d69ef7e5885a3dd6c6a3ea8426bdc28260c50340
Daniel <traron@gmail.com>
2012-10-19 16:32:10 -0700

    - changed timing for motor_test

--- software/atrias_controllers/atc_motor_test/include/atc_motor_test/controller_component.h
+++ software/atrias_controllers/atc_motor_test/include/atc_motor_test/controller_component.h
@@ -35,8 +35,8 @@ using namespace atc_motor_test;
 
 #define MAX_TORQUE       1.0
 #define MIN_TORQUE       0.4
-#define MIN_DURATION_SEC 1.0
-#define MAX_DURATION_SEC 10.0
+#define MIN_DURATION_SEC 0.5
+#define MAX_DURATION_SEC 1.0
 
 namespace atrias {
 namespace controller {
