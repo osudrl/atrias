620ad497ef4e41ac3936e971a0effe291bbee84a
Dynamic Robotics Laboratory <drl@drl-optimus.(none)>
2012-10-24 10:13:36 -0700

    Cleaning up the beginnings of the velocity test controller.

--- software/atrias_controllers/atc_velocity_tuning/include/atc_velocity_tuning/controller_component.h
+++ software/atrias_controllers/atc_velocity_tuning/include/atc_velocity_tuning/controller_component.h
@@ -2,11 +2,11 @@
 #define __ATC_VELOCITY_TUNING_H__
 
 /*! \file controller_component.h
- *  \author Andrew Peekema
+ *  \author Ryan Van Why
  *  \brief Orocos Component header for atc_velocity_tuning controller.
  */
 
-// Orocos 
+// Orocos
 #include <rtt/os/main.h>
 #include <rtt/RTT.hpp>
 #include <rtt/Logger.hpp>
@@ -17,7 +17,6 @@
 // C
 #include <stdlib.h>
 
-#include <atrias_shared/GuiPublishTimer.h>
 #include <atrias_shared/globals.h>
 #include <robot_invariant_defs.h>
 
@@ -38,44 +37,37 @@ using namespace shared;
 namespace controller {
 
 class ATCVelocityTuning : public TaskContext {
-private:
-    // This Operation is called by the RT Operations Manager.
-    atrias_msgs::controller_output runController(atrias_msgs::robot_state rs);
-
-    atrias_msgs::controller_output co;
-
-    // Subcontroller names
-    std::string pd0Name;
-
-    // Subcontroller components
-    TaskContext *pd0; 
-
-    // Service properties
-    Property<double> D0;
-    Property<double> P0;
-
-    // Subcontroller operations
-    OperationCaller<double(double, double, double, double)> pd0RunController;
-
-    // Logging
-    controller_log_data              logData;
-    OutputPort<controller_log_data>  logPort;
-
-    // For the GUI
-    shared::GuiPublishTimer                         *pubTimer;
-    controller_input                                guiIn;
-    InputPort<controller_input>                     guiDataIn;
-
-public:
-    // Constructor
-    ATCVelocityTuning(std::string name);
-
-    // Standard Orocos hooks
-    bool configureHook();
-    bool startHook();
-    void updateHook();
-    void stopHook();
-    void cleanupHook();
+	private:
+		// This Operation is called by the RT Operations Manager.
+		atrias_msgs::controller_output runController(atrias_msgs::robot_state rs);
+		
+		// Subcontroller names
+		std::string pd0Name;
+		
+		// Subcontroller components
+		TaskContext *pd0;
+		
+		// Service properties
+		Property<double> D0;
+		Property<double> P0;
+		
+		// Subcontroller operations
+		OperationCaller<double(double, double, double, double)> pd0RunController;
+		
+		// For the GUI
+		controller_input guiIn;
+		InputPort<controller_input>                     guiDataIn;
+		
+	public:
+		// Constructor
+		ATCVelocityTuning(std::string name);
+		
+		// Standard Orocos hooks
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void stopHook();
+		void cleanupHook();
 };
 }
 }
--- software/atrias_controllers/atc_velocity_tuning/src/controller_component.cpp
+++ software/atrias_controllers/atc_velocity_tuning/src/controller_component.cpp
@@ -1,5 +1,5 @@
 /*! \file controller_component.cpp
- *  \author Andrew Peekema
+ *  \author Ryan Van Why
  *  \brief Orocos Component code for the atc_velocity_tuning controller.
  */
 
@@ -8,100 +8,70 @@
 namespace atrias {
 namespace controller {
 
-ATCVelocityTuning::ATCVelocityTuning(std::string name):
-    RTT::TaskContext(name),
-    logPort(name + "_log"),
-    guiDataIn("gui_data_in")
+ATCVelocityTuning::ATCVelocityTuning(std::string name) :
+	RTT::TaskContext(name),
+	guiDataIn("gui_data_in")
 {
-    this->provides("atc")
-        ->addOperation("runController", &ATCVelocityTuning::runController, this, ClientThread)
-        .doc("Get robot_state from RTOps and return controller output.");
+	this->provides("atc")
+	->addOperation("runController", &ATCVelocityTuning::runController, this, ClientThread)
+	.doc("Get robot_state from RTOps and return controller output.");
 
-    // Add properties
-    this->addProperty("pd0Name", pd0Name);
+	// Add properties
+	this->addProperty("pd0Name", pd0Name);
 
-    // For the GUI
-    addEventPort(guiDataIn);
-    pubTimer = new GuiPublishTimer(20);
-
-    // Logging
-    // Create a port
-    addPort(logPort);
-    // Buffer port so we capture all data.
-    ConnPolicy policy = RTT::ConnPolicy::buffer(100000);
-    // Transport type = ROS
-    policy.transport = 3;
-    // ROS topic name
-    policy.name_id = "/" + name + "_log";
-    // Construct the stream between the port and ROS topic
-    logPort.createStream(policy);
-
-    log(Info) << "[ATCMT] Constructed!" << endlog();
+	// For the GUI
+	addEventPort(guiDataIn);
+	
+	log(Info) << "[ATCMT] Constructed!" << endlog();
 }
 
 atrias_msgs::controller_output ATCVelocityTuning::runController(atrias_msgs::robot_state rs) {
-    // Do nothing unless told otherwise
-    co.lLeg.motorCurrentA   = 0.0;
-    co.lLeg.motorCurrentB   = 0.0;
-    co.lLeg.motorCurrentHip = 0.0;
-    co.rLeg.motorCurrentA   = 0.0;
-    co.rLeg.motorCurrentB   = 0.0;
-    co.rLeg.motorCurrentHip = 0.0;
+	atrias_msgs::controller_output co;
+	
+	co.lLeg.motorCurrentA   = 0.0;
+	co.lLeg.motorCurrentB   = 0.0;
+	co.lLeg.motorCurrentHip = 0.0;
+	co.rLeg.motorCurrentA   = 0.0;
+	co.rLeg.motorCurrentB   = 0.0;
+	co.rLeg.motorCurrentHip = 0.0;
 
-    // Only run the controller when we're enabled
-    if ((uint8_t)rs.cmState != (uint8_t)controllerManager::RtOpsCommand::ENABLE)
-        return co;
+	// Command a run state
+	co.command = medulla_state_run;
 
-    // begin control code //
-
-    // Stuff the msg
-    co.lLeg.motorCurrentA = guiIn.des_motor_torque_A;
-    co.lLeg.motorCurrentB = guiIn.des_motor_torque_B;
-    co.lLeg.motorCurrentHip = guiIn.des_motor_torque_hip;
-
-    // end control code //
-
-    // Command a run state
-    co.command = medulla_state_run;
-
-    // Stuff the msg and push to ROS for logging
-    logData.desiredState = 0.0;
-    logPort.write(logData);
-
-    // Output for RTOps
-    return co;
+	// Output for RTOps
+	return co;
 }
 
 // Don't put control code below here!
 bool ATCVelocityTuning::configureHook() {
-    // Connect to the subcontrollers
-    pd0 = this->getPeer(pd0Name);
-    if (pd0)
-        pd0RunController = pd0->provides("pd")->getOperation("runController");
+	// Connect to the subcontrollers
+	pd0 = this->getPeer(pd0Name);
+	if (pd0)
+		pd0RunController = pd0->provides("pd")->getOperation("runController");
 
-    // Get references to subcontroller component properties
-    D0 = pd0->properties()->getProperty("D");
-    P0 = pd0->properties()->getProperty("P");
+	// Get references to subcontroller component properties
+	D0 = pd0->properties()->getProperty("D");
+	P0 = pd0->properties()->getProperty("P");
 
-    log(Info) << "[ATCMT] configured!" << endlog();
-    return true;
+	log(Info) << "[ATCMT] configured!" << endlog();
+	return true;
 }
 
 bool ATCVelocityTuning::startHook() {
-    log(Info) << "[ATCMT] started!" << endlog();
-    return true;
+	log(Info) << "[ATCMT] started!" << endlog();
+	return true;
 }
 
 void ATCVelocityTuning::updateHook() {
-    guiDataIn.read(guiIn);
+	guiDataIn.read(guiIn);
 }
 
 void ATCVelocityTuning::stopHook() {
-    log(Info) << "[ATCMT] stopped!" << endlog();
+	log(Info) << "[ATCMT] stopped!" << endlog();
 }
 
 void ATCVelocityTuning::cleanupHook() {
-    log(Info) << "[ATCMT] cleaned up!" << endlog();
+	log(Info) << "[ATCMT] cleaned up!" << endlog();
 }
 
 ORO_CREATE_COMPONENT(ATCVelocityTuning)
