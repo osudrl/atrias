1b939385f18bd939c87dfaa111ca60bfd687e996
yoos <yoos117@gmail.com>
2012-10-30 13:16:02 -0700

    Implement GUI code.
    Untested code; may not compile!

--- software/atrias_controllers/atc_eq_point/include/atc_eq_point/controller_gui.h
+++ software/atrias_controllers/atc_eq_point/include/atc_eq_point/controller_gui.h
@@ -24,19 +24,25 @@ atc_eq_point::controller_input controllerDataOut;
 atc_eq_point::controller_status controllerDataIn;
 
 // GUI elements
-Gtk::HScale *torque_A_hscale,
-        *torque_B_hscale,
-        *torque_hip_hscale;
+Gtk::ComboBox *control_combobox;
 
-Gtk::CheckButton *set_position_checkbutton;
+Gtk::Button *gc_l_button,
+	*gc_r_button;
 
-// Parameters
-//     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
-//     arise when both C and python nodes access the same parameter. See:
-//     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
-double torque_A_param;
-double torque_B_param;
-double torque_hip_param;
+Gtk::SpinButton *aea_spinbutton,
+	*pea_spinbutton,
+	*l_leg_fl_spinbutton,
+	*p_af_spinbutton,
+	*d_af_spinbutton,
+	*p_lf_spinbutton,
+	*d_lf_spinbutton,
+	*l_fl_spinbutton,
+	*l_leg_st_spinbutton,
+	*p_as_spinbutton,
+	*d_as_spinbutton,
+	*p_ls_spinbutton,
+	*d_ls_spinbutton,
+	*l_st_spinbutton;
 
 void controllerCallback(const atc_eq_point::controller_status &status);
 
--- software/atrias_controllers/atc_eq_point/src/controller_gui.cpp
+++ software/atrias_controllers/atc_eq_point/src/controller_gui.cpp
@@ -4,62 +4,95 @@
  * atc_eq_point controller
  *
  *  Created on: May 5, 2012
- *      Author: Michael Anderson
+ *	  Author: Michael Anderson
  */
 
 #include <atc_eq_point/controller_gui.h>
 
 //! \brief Initialize the GUI.
 bool guiInit(Glib::RefPtr<Gtk::Builder> gui) {
-    gui->get_widget("torque_A_hscale", torque_A_hscale);
-    gui->get_widget("torque_B_hscale", torque_B_hscale);
-    gui->get_widget("torque_hip_hscale", torque_hip_hscale);
+	gui->get_widget("control_combobox", control_combobox);
+	gui->get_widget("gc_l_button", gc_l_button);
+	gui->get_widget("gc_r_button", gc_r_button);
+	gui->get_widget("aea_spinbutton", aea_spinbutton);
+	gui->get_widget("pea_spinbutton", pea_spinbutton);
+	gui->get_widget("l_leg_fl_spinbutton", l_leg_fl_spinbutton);
+	gui->get_widget("p_af_spinbutton", p_af_spinbutton);
+	gui->get_widget("d_af_spinbutton", d_af_spinbutton);
+	gui->get_widget("p_lf_spinbutton", p_lf_spinbutton);
+	gui->get_widget("d_lf_spinbutton", d_lf_spinbutton);
+	gui->get_widget("l_fl_spinbutton", l_fl_spinbutton);
+	gui->get_widget("l_leg_st_spinbutton", l_leg_st_spinbutton);
+	gui->get_widget("p_as_spinbutton", p_as_spinbutton);
+	gui->get_widget("d_as_spinbutton", d_as_spinbutton);
+	gui->get_widget("p_ls_spinbutton", p_ls_spinbutton);
+	gui->get_widget("d_ls_spinbutton", d_ls_spinbutton);
+	gui->get_widget("l_st_spinbutton", l_st_spinbutton);
 
-    if (torque_A_hscale && torque_B_hscale && torque_hip_hscale) {
-        // Set ranges.
-        torque_A_hscale->set_range(-10., 10.);
-        torque_B_hscale->set_range(-10., 10.);
-        torque_hip_hscale->set_range(-10., 10.);
+	if (control_combobox && gc_l_button && gc_r_button &&
+			aea_spinbutton && pea_spinbutton &&
+			l_leg_fl_spinbutton && p_af_spinbutton && d_af_spinbutton p_lf_spinbutton && d_lf_spinbutton && l_fl_spinbutton &&
+			l_leg_st_spinbutton && p_as_spinbutton && d_as_spinbutton p_ls_spinbutton && d_ls_spinbutton && l_st_spinbutton) {
+		// Set ranges.
+		aea_spinbutton->set_range(0.5, 1.5);
+		pea_spinbutton->set_range(1.5, 2.5);
+		l_leg_fl_spinbutton->set_range(0.7, 1.0);
+		p_af_spinbutton->set_range(0, 5000);
+		d_af_spinbutton->set_range(0, 500);
+		p_lf_spinbutton->set_range(0, 5000);
+		d_lf_spinbutton->set_range(0, 500);
+		l_fl_spinbutton->set_range(0, 60);
+		l_leg_st_spinbutton->set_range(0.7, 1.0);
+		p_as_spinbutton->set_range(0, 5000);
+		d_as_spinbutton->set_range(0, 500);
+		p_ls_spinbutton->set_range(0, 5000);
+		d_ls_spinbutton->set_range(0, 500);
+		l_st_spinbutton->set_range(0, 60);
 
-        // Set up subscriber and publisher.
-        sub = nh.subscribe("atc_eq_point_status", 0, controllerCallback);
-        pub = nh.advertise<atc_eq_point::controller_input>("atc_eq_point_input", 0);
-        return true;
-    }
-    return false;
+		// Set up subscriber and publisher.
+		sub = nh.subscribe("atc_eq_point_status", 0, controllerCallback);
+		pub = nh.advertise<atc_eq_point::controller_input>("atc_eq_point_input", 0);
+		return true;
+	}
+	return false;
 }
 
 //! \brief Update our local copy of the controller status.
 void controllerCallback(const atc_eq_point::controller_status &status) {
-    controllerDataIn = status;
+	controllerDataIn = status;
 }
 
 //! \brief Get parameters from the server and configure GUI accordingly.
 void getParameters() {
-    // Get parameters in the atrias_gui namespace.
-    nh.getParam("/atrias_gui/torque_A", torque_A_param);
-    nh.getParam("/atrias_gui/torque_B", torque_B_param);
-    nh.getParam("/atrias_gui/torque_hip", torque_hip_param);
-
-    // Configure the GUI.
-    torque_A_hscale->set_value(torque_A_param);
-    torque_B_hscale->set_value(torque_B_param);
-    torque_hip_hscale->set_value(torque_hip_param);
+	// Disabled.
 }
 
 //! \brief Set parameters on server according to current GUI settings.
 void setParameters() {
-    nh.setParam("/atrias_gui/torque_A", torque_A_param);
-    nh.setParam("/atrias_gui/torque_B", torque_B_param);
-    nh.setParam("/atrias_gui/torque_hip", torque_hip_param);
+	// Disabled.
 }
 
 //! \brief Update the GUI.
 void guiUpdate() {
-    controllerDataOut.des_motor_torque_A   = torque_A_param   = torque_A_hscale->get_value();
-    controllerDataOut.des_motor_torque_B   = torque_B_param   = torque_B_hscale->get_value();
-    controllerDataOut.des_motor_torque_hip = torque_hip_param = torque_hip_hscale->get_value();
-    pub.publish(controllerDataOut);
+	controllerDataOut.control  = control_combobox->get_active_row_number();
+	controllerDataOut.gc_l     = gc_l_button->get_active();
+	controllerDataOut.gc_r     = gc_r_button->get_active();
+	controllerDataOut.aea      = aea_spinbutton->get_value();
+	controllerDataOut.pea      = pea_spinbutton->get_value();
+	controllerDataOut.l_leg_fl = l_leg_fl_spinbutton->get_value();
+	controllerDataOut.p_af     = p_af_spinbutton->get_value();
+	controllerDataOut.d_af     = d_af_spinbutton->get_value();
+	controllerDataOut.p_lf     = p_lf_spinbutton->get_value();
+	controllerDataOut.d_lf     = d_lf_spinbutton->get_value();
+	controllerDataOut.l_fl     = l_fl_spinbutton->get_value();
+	controllerDataOut.l_leg_st = l_leg_st_spinbutton->get_value();
+	controllerDataOut.p_as     = p_as_spinbutton->get_value();
+	controllerDataOut.d_as     = d_as_spinbutton->get_value();
+	controllerDataOut.p_ls     = p_ls_spinbutton->get_value();
+	controllerDataOut.d_ls     = d_ls_spinbutton->get_value();
+	controllerDataOut.l_st     = l_st_spinbutton->get_value();
+
+	pub.publish(controllerDataOut);
 }
 
 //! \brief Take down the GUI.
