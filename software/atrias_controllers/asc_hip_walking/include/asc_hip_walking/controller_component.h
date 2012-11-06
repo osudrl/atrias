331fec07439fe0c24fa9f8a7975a6eb8c63e39ed
Ryan Van Why <jrvanwhy@gmail.com>
2012-11-05 19:02:21 -0800

    Beginning walking hip controller.

---
+++ software/atrias_controllers/asc_hip_walking/include/asc_hip_walking/controller_component.h
@@ -0,0 +1,57 @@
+#ifndef __ASC_HIP_WALKING_H__
+#define __ASC_HIP_WALKING_H__
+
+/*! \file controller_component.h
+ *  \author Andrew Peekema
+ *  \brief Orocos Component header for the asc_hip_walking subcontroller.
+ */
+
+// Orocos 
+#include <rtt/os/main.h>
+#include <rtt/RTT.hpp>
+#include <rtt/Logger.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/Component.hpp>
+
+// C
+#include <stdlib.h>
+
+#include <robot_invariant_defs.h>
+
+// Datatypes
+#include <asc_hip_walking/controller_log_data.h>
+
+using namespace RTT;
+using namespace Orocos;
+using namespace asc_hip_walking;
+
+namespace atrias {
+namespace controller {
+
+class ASCHipWalking : public TaskContext {
+private:
+    // Operations
+    double runController(double exampleInput);
+
+    double out;
+
+    // Logging
+    controller_log_data logData;
+    OutputPort<controller_log_data> logPort;
+
+public:
+    // Constructor
+    ASCHipWalking(std::string name);
+
+    // Standard Orocos hooks
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+};
+
+}
+}
+
+#endif
---
+++ software/atrias_controllers/asc_hip_walking/src/controller_component.cpp
@@ -0,0 +1,71 @@
+/*! \file controller_component.cpp
+ *  \author Andrew Peekema
+ *  \brief Orocos Component code for the asc_hip_walking subcontroller.
+ */
+
+#include <asc_hip_walking/controller_component.h>
+
+namespace atrias {
+namespace controller {
+
+ASCHipWalking::ASCHipWalking(std::string name):
+    RTT::TaskContext(name),
+    logPort(name + "_log")
+{
+    this->provides("exampleService")
+        ->addOperation("runController", &ASCHipWalking::runController, this, ClientThread)
+        .doc("Run the controller.");
+
+    // Logging
+    // Create a port
+    addPort(logPort); 
+    // Connect with buffer size 100000 so we get all data.
+    ConnPolicy policy = RTT::ConnPolicy::buffer(100000);
+    // Transport type = ROS
+    policy.transport = 3;
+    // ROS topic name
+    policy.name_id = "/" + name + "_log";
+    // Construct the stream between the port and ROS topic
+    logPort.createStream(policy);
+
+    log(Info) << "[ASCHipWalking] Constructed!" << endlog();
+}
+
+// Put control code here.
+double ASCHipWalking::runController(double exampleInput) {
+    out = exampleInput;
+
+    // Stuff the msg and push to ROS for logging
+    logData.input = exampleInput;
+    logData.output = out;
+    logPort.write(logData);
+
+    // Output for the parent controller
+    return out;
+}
+
+bool ASCHipWalking::configureHook() {
+    log(Info) << "[ASCHipWalking] configured!" << endlog();
+    return true;
+}
+
+bool ASCHipWalking::startHook() {
+    log(Info) << "[ASCHipWalking] started!" << endlog();
+    return true;
+}
+
+void ASCHipWalking::updateHook() {
+}
+
+void ASCHipWalking::stopHook() {
+    log(Info) << "[ASCHipWalking] stopped!" << endlog();
+}
+
+void ASCHipWalking::cleanupHook() {
+    log(Info) << "[ASCHipWalking] cleaned up!" << endlog();
+}
+
+ORO_CREATE_COMPONENT(ASCHipWalking)
+
+}
+}
