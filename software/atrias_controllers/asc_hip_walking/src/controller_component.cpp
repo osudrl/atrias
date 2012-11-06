a4ab373bd98d2d7856379b488667a0c2180e1d0d
Ryan Van Why <jrvanwhy@gmail.com>
2012-11-05 19:06:43 -0800

    Beginning hip walking controller cleanup.

--- software/atrias_controllers/asc_hip_walking/src/controller_component.cpp
+++ software/atrias_controllers/asc_hip_walking/src/controller_component.cpp
@@ -8,61 +8,61 @@
 namespace atrias {
 namespace controller {
 
-ASCHipWalking::ASCHipWalking(std::string name):
-    RTT::TaskContext(name),
-    logPort(name + "_log")
+ASCHipWalking::ASCHipWalking(std::string name) :
+	RTT::TaskContext(name),
+	logPort(name + "_log")
 {
-    this->provides("exampleService")
-        ->addOperation("runController", &ASCHipWalking::runController, this, ClientThread)
-        .doc("Run the controller.");
+	this->provides("exampleService")
+	->addOperation("runController", &ASCHipWalking::runController, this, ClientThread)
+	.doc("Run the controller.");
 
-    // Logging
-    // Create a port
-    addPort(logPort); 
-    // Connect with buffer size 100000 so we get all data.
-    ConnPolicy policy = RTT::ConnPolicy::buffer(100000);
-    // Transport type = ROS
-    policy.transport = 3;
-    // ROS topic name
-    policy.name_id = "/" + name + "_log";
-    // Construct the stream between the port and ROS topic
-    logPort.createStream(policy);
+	// Logging
+	// Create a port
+	addPort(logPort);
+	// Connect with buffer size 100000 so we get all data.
+	ConnPolicy policy = RTT::ConnPolicy::buffer(100000);
+	// Transport type = ROS
+	policy.transport = 3;
+	// ROS topic name
+	policy.name_id = "/" + name + "_log";
+	// Construct the stream between the port and ROS topic
+	logPort.createStream(policy);
 
-    log(Info) << "[ASCHipWalking] Constructed!" << endlog();
+	log(Info) << "[ASCHipWalking] Constructed!" << endlog();
 }
 
 // Put control code here.
 double ASCHipWalking::runController(double exampleInput) {
-    out = exampleInput;
+	out = exampleInput;
 
-    // Stuff the msg and push to ROS for logging
-    logData.input = exampleInput;
-    logData.output = out;
-    logPort.write(logData);
+	// Stuff the msg and push to ROS for logging
+	logData.input = exampleInput;
+	logData.output = out;
+	logPort.write(logData);
 
-    // Output for the parent controller
-    return out;
+	// Output for the parent controller
+	return out;
 }
 
 bool ASCHipWalking::configureHook() {
-    log(Info) << "[ASCHipWalking] configured!" << endlog();
-    return true;
+	log(Info) << "[ASCHipWalking] configured!" << endlog();
+	return true;
 }
 
 bool ASCHipWalking::startHook() {
-    log(Info) << "[ASCHipWalking] started!" << endlog();
-    return true;
+	log(Info) << "[ASCHipWalking] started!" << endlog();
+	return true;
 }
 
 void ASCHipWalking::updateHook() {
 }
 
 void ASCHipWalking::stopHook() {
-    log(Info) << "[ASCHipWalking] stopped!" << endlog();
+	log(Info) << "[ASCHipWalking] stopped!" << endlog();
 }
 
 void ASCHipWalking::cleanupHook() {
-    log(Info) << "[ASCHipWalking] cleaned up!" << endlog();
+	log(Info) << "[ASCHipWalking] cleaned up!" << endlog();
 }
 
 ORO_CREATE_COMPONENT(ASCHipWalking)
