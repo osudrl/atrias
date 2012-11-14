4349d3a421bb6d72af6d3cd2e9260f9bd50052b4
Ryan Van Why <jrvanwhy@gmail.com>
2012-11-14 12:02:16 -0800

    Cleaning up asc_spring_torque/controller_component.h

--- software/atrias_controllers/asc_spring_torque/include/asc_spring_torque/controller_component.h
+++ software/atrias_controllers/asc_spring_torque/include/asc_spring_torque/controller_component.h
@@ -2,11 +2,11 @@
 #define __ASC_SPRING_TORQUE_H__
 
 /*! \file controller_component.h
- *  \author Andrew Peekema
+ *  \author Ryan Van Why
  *  \brief Orocos Component header for the asc_spring_torque subcontroller.
  */
 
-// Orocos 
+// Orocos
 #include <rtt/os/main.h>
 #include <rtt/RTT.hpp>
 #include <rtt/Logger.hpp>
@@ -29,40 +29,36 @@ namespace atrias {
 namespace controller {
 
 class ASCSpringTorque : public TaskContext {
-private:
-    // Operations
-    double runController(double exampleInput);
-
-    double out;
-
-    // Subcontroller names
-    std::string linearInterp0Name;
-
-    // Subcontroller components
-    TaskContext *linearInterp0;
-
-    // Service properties
-
-    // Subcontroller operations
-    OperationCaller<void(double samples[], int, double, double)> linearInterp0InputPoints;
-    OperationCaller<double(double)> linearInterp0GetValue;
-    OperationCaller<void(double samples[], int, double, double)> linearInterp0InputPoints;
-    OperationCaller<double(double)> linearInterp0GetValue;
-
-    // Logging
-    controller_log_data logData;
-    OutputPort<controller_log_data> logPort;
-
-public:
-    // Constructor
-    ASCSpringTorque(std::string name);
-
-    // Standard Orocos hooks
-    bool configureHook();
-    bool startHook();
-    void updateHook();
-    void stopHook();
-    void cleanupHook();
+	private:
+		// Operations
+		double runController(double exampleInput);
+		
+		// Subcontroller names
+		std::string linearInterp0Name;
+		
+		// Subcontroller components
+		TaskContext *linearInterp0;
+		
+		// Service properties
+		
+		// Subcontroller operations
+		OperationCaller<void(double samples[], int, double, double)> linearInterp0InputPoints;
+		OperationCaller<double(double)> linearInterp0GetValue;
+		
+		// Logging
+		controller_log_data             logData;
+		OutputPort<controller_log_data> logPort;
+		
+	public:
+		// Constructor
+		ASCSpringTorque(std::string name);
+		
+		// Standard Orocos hooks
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void stopHook();
+		void cleanupHook();
 };
 
 }
