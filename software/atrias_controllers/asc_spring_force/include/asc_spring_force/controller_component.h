f39df2359756dbe16af304ee3989e8988ffdaab8
Ryan Van Why <jrvanwhy@gmail.com>
2012-11-15 14:32:46 -0800

    Cleaning up asc_spring_force's controller_component.

--- software/atrias_controllers/asc_spring_force/include/asc_spring_force/controller_component.h
+++ software/atrias_controllers/asc_spring_force/include/asc_spring_force/controller_component.h
@@ -2,11 +2,11 @@
 #define __ASC_SPRING_FORCE_H__
 
 /*! \file controller_component.h
- *  \author Andrew Peekema
+ *  \author Ryan Van Why
  *  \brief Orocos Component header for the asc_spring_force subcontroller.
  */
 
-// Orocos 
+// Orocos
 #include <rtt/os/main.h>
 #include <rtt/RTT.hpp>
 #include <rtt/Logger.hpp>
@@ -29,42 +29,40 @@ namespace atrias {
 namespace controller {
 
 class ASCSpringForce : public TaskContext {
-private:
-    // Operations
-    double runController(double exampleInput);
-
-    double out;
-
-    // Subcontroller names
-    std::string springTorque0Name;
-    std::string springTorque1Name;
-
-    // Subcontroller components
-    TaskContext *springTorque0;
-    TaskContext *springTorque1;
-
-    // Service properties
-    Property<std::string> linearInterp0Name0;
-    Property<std::string> linearInterp0Name1;
-
-    // Subcontroller operations
-    OperationCaller<double(double)> springTorque0GetTorque;
-    OperationCaller<double(double)> springTorque1GetTorque;
-
-    // Logging
-    controller_log_data logData;
-    OutputPort<controller_log_data> logPort;
-
-public:
-    // Constructor
-    ASCSpringForce(std::string name);
-
-    // Standard Orocos hooks
-    bool configureHook();
-    bool startHook();
-    void updateHook();
-    void stopHook();
-    void cleanupHook();
+	private:
+		// Operations
+		double getForce(double motorAAngle, double legAAngle, double motorBAngle, double legBAngle);;
+		
+		// Subcontroller names
+		std::string springTorque0Name;
+		std::string springTorque1Name;
+		
+		// Subcontroller components
+		TaskContext *springTorque0;
+		TaskContext *springTorque1;
+		
+		// Service properties
+		Property<std::string> linearInterp0Name0;
+		Property<std::string> linearInterp0Name1;
+		
+		// Subcontroller operations
+		OperationCaller<double(double)> springTorque0GetTorque;
+		OperationCaller<double(double)> springTorque1GetTorque;
+		
+		// Logging
+		controller_log_data logData;
+		OutputPort<controller_log_data> logPort;
+		
+	public:
+		// Constructor
+		ASCSpringForce(std::string name);
+		
+		// Standard Orocos hooks
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void stopHook();
+		void cleanupHook();
 };
 
 }
