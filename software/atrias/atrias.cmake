45e9d2ef5c8de0c935b7c34892856edbe2ac179b
yoos <yoos117@gmail.com>
2012-11-27 14:56:09 -0800

    Further simplify CMake inclusion.

--- software/atrias/atrias.cmake
+++ software/atrias/atrias.cmake
@@ -6,22 +6,34 @@ set(ATRIAS_BUILD_CONTROLLERS 0)
 
 ### DO NOT EDIT BELOW THIS LINE ###
 
+# Include robot_definitions by guessing (relative to the directory of the
+# project including this CMake file. This is shameful. There has to be a better
+# way besides guessing.
+include_directories(../../robot_definitions)
+include_directories(../../../robot_definitions)
+
 # Check that GTK libraries exist. If not, do not try to build the GUI.
 if(ATRIAS_BUILD_GUI)
 	find_package(GTK2 2.4 COMPONENTS gtk gtkmm REQUIRED)
-	if(NOT GTK2_FOUND)
+	if(GTK2_FOUND)
+		include_directories(${GTK2_INCLUDE_DIRS})
+	else(GTK2_FOUND)
 		set(ATRIAS_BUILD_GUI 0)
 		message(ERROR "Could not find GTK libraries. Not building GUI elements.")
-	endif(NOT GTK2_FOUND)
+	endif(GTK2_FOUND)
 endif(ATRIAS_BUILD_GUI)
 
 # Check that Orocos RTT exists. If not, do not try to build the controller
 # components.
 if(ATRIAS_BUILD_CONTROLLERS)
 	rosbuild_find_ros_package(rtt)
-	if(NOT DEFINED rtt_PACKAGE_PATH)
+	if(DEFINED rtt_PACKAGE_PATH)
+		set(RTT_HINTS HINTS ${rtt_PACKAGE_PATH}/../install)
+		find_package(OROCOS-RTT REQUIRED ${RTT_HINTS})
+		include(${OROCOS-RTT_USE_FILE_PATH}/UseOROCOS-RTT.cmake)
+	else(DEFINED rtt_PACKAGE_PATH)
 		set(ATRIAS_BUILD_CONTROLLERS 0)
 		message(ERROR "Could not find rtt. Not building controller components.")
-	endif(NOT DEFINED rtt_PACKAGE_PATH)
+	endif(DEFINED rtt_PACKAGE_PATH)
 endif(ATRIAS_BUILD_CONTROLLERS)
 
