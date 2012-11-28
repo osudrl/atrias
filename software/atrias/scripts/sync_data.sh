d0960f087a611e875c1e4036043ec7a7d5dffc09
yoos <yoos117@gmail.com>
2012-11-28 10:25:12 -0800

    Update syncdata.sh target directory.

--- software/atrias/scripts/sync_data.sh
+++ software/atrias/scripts/sync_data.sh
@@ -6,7 +6,7 @@ GUI_COMPUTER="cov022-5"
 if [ $HOSTNAME == $GUI_COMPUTER ]; then
     rsync -avz --progress -e ssh drl@$ROBOT:/home/drl/atrias/software/atrias/bagfiles/*.bag `rospack find atrias`/bagfiles
 elif [ $HOSTNAME == $ROBOT ]; then
-    rsync -avz --progress `rospack find atrias`/bagfiles/*.bag -e ssh drl@$GUI_COMPUTER:/home/drl/git/atrias/software/atrias/bagfiles
+    rsync -avz --progress `rospack find atrias`/bagfiles/*.bag -e ssh drl@$GUI_COMPUTER:/home/drl/atrias/software/atrias/bagfiles
 else
     echo "Run this from either the GUI computer or the robot!"
 fi
