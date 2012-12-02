59e093abf92eeecd37c628c4126ac803eac5da0c
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-02 14:18:42 -0800

    Spring tester data parsing script almost done... just need to make it write to file now.

--- software/atrias_controllers/asc_spring_torque/src/parse_data.py
+++ software/atrias_controllers/asc_spring_torque/src/parse_data.py
@@ -12,19 +12,27 @@ from numpy.linalg import lstsq
 outfile = open(sys.argv[2], 'w')
 
 datapairs   = [line.split() for line in open(sys.argv[1])]
-torques     = [[pair[0]]    for pair in datapairs]
-deflections = [[pair[1]]    for pair in datapairs]
+torques     = [[float(pair[0])]    for pair in datapairs]
+deflections = [[float(pair[1])]    for pair in datapairs]
 
 print('''
 #ifndef PROCESSEDDATA_H
 #define PROCESSEDDATA_H
-
 ''')
 
 print('#DEFINE NUM_TORQUE_SAMPLES 2')
 # The indexes pull the exact value we need from the regression function's output
-print(max(deflections))
-print('#DEFINE TORQUE_SAMPLES     { 0.0, ' + str(lstsq(deflections, torques)[0][0][0] * max(deflections)[0]))
-print('#DEFINE MAX_DEFLECTION     ' + str(max(deflections)))
+print('#DEFINE TORQUE_SAMPLES     {0.0, ' + str(lstsq(deflections, torques)[0][0][0] * max(deflections)[0]) + '}')
+print('#DEFINE MAX_DEFLECTION     ' + str(max(deflections)[0]))
+
+print
+
+print('#DEFINE NUM_DEFL_SAMPLES   2')
+print('#DEFINE DEFL_SAMPLES       {0.0, ' + str(lstsq(torques, deflections)[0][0][0] * max(torques)[0])     + '}')
+print('#DEFINE MAX_TORQUE         ' + str(max(torques)[0]))
+
+print('''
+#endif // PROCESSEDDATA_H
+''')
 
 outfile.close()
