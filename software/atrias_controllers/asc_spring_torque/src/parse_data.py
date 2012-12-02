86c67b6894e6d0774387971d1303f213d1407e77
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-02 13:56:36 -0800

    More spring torque asc work.

--- software/atrias_controllers/asc_spring_torque/src/parse_data.py
+++ software/atrias_controllers/asc_spring_torque/src/parse_data.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python2
 
+# This script parses in the spring tester data and outputs a .h
+# file containing arrays and sizes for a linear interpolation
+# controller. The first parameter should be file with the data
+# and the second should be the header file into which
+# to dump the data.
+
 import sys
-import os
+from numpy.linalg import lstsq
 
-print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-print sys.argv
-print os.getcwd()
-print "################################"
+outfile = open(sys.argv[2], 'w')
 
-file = open(sys.argv[2], 'w')
-file.write("Stuffz\n")
-file.close()
+datapairs   = [line.split() for line in open(sys.argv[1])]
+torques     = [[pair[0]]    for pair in datapairs]
+deflections = [[pair[1]]    for pair in datapairs]
+
+print('''
+#ifndef PROCESSEDDATA_H
+#define PROCESSEDDATA_H
+
+''')
+
+print('#DEFINE NUM_TORQUE_SAMPLES 2')
+# The indexes pull the exact value we need from the regression function's output
+print(max(deflections))
+print('#DEFINE TORQUE_SAMPLES     { 0.0, ' + str(lstsq(deflections, torques)[0][0][0] * max(deflections)[0]))
+print('#DEFINE MAX_DEFLECTION     ' + str(max(deflections)))
+
+outfile.close()
