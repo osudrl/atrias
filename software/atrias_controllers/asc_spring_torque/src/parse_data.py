aa629e4c63d1fc35a7f61bc84dc3417c453a8e86
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-02 14:26:45 -0800

    Finished data parsing script!

--- software/atrias_controllers/asc_spring_torque/src/parse_data.py
+++ software/atrias_controllers/asc_spring_torque/src/parse_data.py
@@ -15,23 +15,22 @@ datapairs   = [line.split() for line in open(sys.argv[1])]
 torques     = [[float(pair[0])]    for pair in datapairs]
 deflections = [[float(pair[1])]    for pair in datapairs]
 
-print('''
+outfile.write('''
 #ifndef PROCESSEDDATA_H
 #define PROCESSEDDATA_H
+
 ''')
 
-print('#DEFINE NUM_TORQUE_SAMPLES 2')
+outfile.write('#DEFINE NUM_TORQUE_SAMPLES 2\n')
 # The indexes pull the exact value we need from the regression function's output
-print('#DEFINE TORQUE_SAMPLES     {0.0, ' + str(lstsq(deflections, torques)[0][0][0] * max(deflections)[0]) + '}')
-print('#DEFINE MAX_DEFLECTION     ' + str(max(deflections)[0]))
+outfile.write('#DEFINE TORQUE_SAMPLES     {0.0, ' + str(lstsq(deflections, torques)[0][0][0] * max(deflections)[0]) + '}\n')
+outfile.write('#DEFINE MAX_DEFLECTION     ' + str(max(deflections)[0]) + '\n')
 
-print
+outfile.write('#DEFINE NUM_DEFL_SAMPLES   2\n')
+outfile.write('#DEFINE DEFL_SAMPLES       {0.0, ' + str(lstsq(torques, deflections)[0][0][0] * max(torques)[0])     + '}\n')
+outfile.write('#DEFINE MAX_TORQUE         ' + str(max(torques)[0]) + '\n')
 
-print('#DEFINE NUM_DEFL_SAMPLES   2')
-print('#DEFINE DEFL_SAMPLES       {0.0, ' + str(lstsq(torques, deflections)[0][0][0] * max(torques)[0])     + '}')
-print('#DEFINE MAX_TORQUE         ' + str(max(torques)[0]))
-
-print('''
+outfile.write('''
 #endif // PROCESSEDDATA_H
 ''')
 
