88d42a65da75a045aeac2f94be9f5c799e897356
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-10 14:23:16 -0800

    Okay, beginning C++-based component loading.
    Should probably discuss this with Soo-Hyun and Andrew too, but they're not here now...

---
+++ software/atrias_shared/include/atrias_shared/SubController.hpp
@@ -0,0 +1,45 @@
+#ifndef SUBCONTROLLER_HPP
+#define SUBCONTROLLER_HPP
+
+/** @file SubController.hpp
+  * @author Ryan Van Why
+  * @brief This class represents a subcontroller to
+  *        higher-level controllers.
+  */
+
+// Standard libs
+#include <string>
+
+// Orocos
+#include <rtt/Service.hpp>
+#include <rtt/TaskContext.hpp>
+
+namespace atrias {
+namespace controller {
+
+class SubController {
+	public:
+		/** @brief Allows the controller to access this subcontroller's services.
+		  * @param name The name of this service
+		  * @return A pointer to this service.
+		  */
+		RTT::Service::shared_ptr getService(std::string name);
+
+		/** @brief              Loads and configures this subcontroller.
+		  * @param task_context A pointer to this component's TaskContext. For interacting
+		  *                     with the deployer and other components.
+		  * @param type         The name of the main class of this subcontroller.
+		  */
+		void loadController(RTT::TaskContext* task_context, std::string type);
+
+		/** @brief Cleans up this class and the subcontroller.
+		  */
+		~SubController();
+};
+
+}
+}
+
+#endif // SUBCONTROLLER_HPP
+
+// vim: noexpandtab
---
+++ software/atrias_shared/src/SubController.cpp
@@ -0,0 +1,21 @@
+#include "atrias_shared/SubController.hpp"
+
+namespace atrias {
+namespace controller{
+
+RTT::Service::shared_ptr SubController::getService(std::string name) {
+	log(RTT::Info) << "getService" << RTT::endlog();
+}
+
+void SubController::loadController(RTT::TaskContext* task_context, std::string type) {
+	log(RTT::Info) << "loadController" << RTT::endlog();
+}
+
+SubController::~SubController() {
+	log(RTT::Info) << "destructor" << RTT::endlog();
+}
+
+}
+}
+
+// vim: noexpandtab
