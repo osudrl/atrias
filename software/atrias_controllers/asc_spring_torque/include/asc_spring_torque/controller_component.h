6620932e654605a4a7f2a3aa8f688d159dffaac6
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-11 10:38:21 -0800

    Again, fixing asc_spring_torque after a ComponentLoader refactor.

--- software/atrias_controllers/asc_spring_torque/include/asc_spring_torque/controller_component.h
+++ software/atrias_controllers/asc_spring_torque/include/asc_spring_torque/controller_component.h
@@ -18,7 +18,7 @@
 
 // Our stuff
 #include <asc_spring_torque/controller_log_data.h>
-#include <atrias_shared/Controller.hpp>
+#include <atrias_component_loader/ComponentLoader.hpp>
 
 // Actual data.
 #include "processed_data.h"
@@ -37,8 +37,8 @@ class ASCSpringTorque : public TaskContext {
 		double getDeflection(double Torque);
 
 		// These load and unload our subcontrollers.
-		Controller linearInterp0SubCont;
-		Controller linearInterp1SubCont;
+		ComponentLoader::ComponentLoader linearInterp0SubCont;
+		ComponentLoader::ComponentLoader linearInterp1SubCont;
 		
 		// Subcontroller names
 		// This is the deflection->torque interpolator
