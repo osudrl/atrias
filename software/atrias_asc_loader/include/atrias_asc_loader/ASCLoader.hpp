e7cbd1450068a2bdd1a02a1ea3ddba6e2b50e2c0
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-12 14:01:18 -0800

    Okay, ASCLoader is now loaded with error checking.

--- software/atrias_asc_loader/include/atrias_asc_loader/ASCLoader.hpp
+++ software/atrias_asc_loader/include/atrias_asc_loader/ASCLoader.hpp
@@ -37,18 +37,37 @@ class ASCLoader {
 		~ASCLoader();
 	
 	private:
-		/** @brief This references our subcontroller's component.
-		  * Used to access it for init and shutdown.
-		  */
-		RTT::TaskContext* subcontroller;
-
 		/** @brief The deployer. Lets us do lots of stuff.
 		  */
 		RTT::TaskContext* deployer;
 
+		/** @brief Outputs an error message to the console.
+		  * @param message      The specific error message for the console
+		  * @param task_context The TaskContext pointer passed in
+		  * @param package      The package passed in
+		  * @param type         The type passed in
+		  * Used during component loading
+		  */
+		void reportLoadError(std::string       message,
+		                     RTT::TaskContext* task_context,
+		                     std::string       package,
+		                     std::string       type);
+
+		/** @brief Outputs an error message to the console.
+		  * @param message The error message
+		  * Used during component unloading.
+		  * subcontroller must not be NULL here!
+		  */
+		void reportUnloadError(std::string message);
+
 		/** @brief The component's name. Needed to unload the component.
 		  */
 		std::string name;
+
+		/** @brief This references our subcontroller's component.
+		  * Used to access it for init and shutdown.
+		  */
+		RTT::TaskContext* subcontroller;
 };
 
 }
--- software/atrias_asc_loader/src/ASCLoader.cpp
+++ software/atrias_asc_loader/src/ASCLoader.cpp
@@ -8,43 +8,113 @@ ASCLoader::ASCLoader() {
 }
 
 RTT::TaskContext* ASCLoader::load(RTT::TaskContext* task_context, std::string package, std::string type) {
+	// Sanity checks
+	if (!task_context) {
+		reportLoadError("Null task_context!", task_context, package, type);
+		return nullptr;
+	}
+	if (package.empty()) {
+		reportLoadError("Empty package name given!", task_context, package, type);
+		return nullptr;
+	}
+	if (type.empty()) {
+		reportLoadError("Empty component type given!", task_context, package, type);
+		return nullptr;
+	}
+
+	// Verify that we haven't been run before.
+	if (subcontroller) {
+		reportLoadError("load() has already been called!", task_context, package, type);
+		return nullptr;
+	}
+
 	// Obtain access to the deployer
 	deployer = task_context->getPeer("Deployer");
+	if (!deployer) {
+		reportLoadError("Unable to access the deployer!", task_context, package, type);
+		return nullptr;
+	}
 
 	// Let's import this package, so the deployer recognizes the component type.
 	RTT::OperationCaller<bool(std::string)> import;
 	import = deployer->getOperation("import");
-	import(package);
+	if (!import.ready()) {
+		reportLoadError("Could not access deployer's import operation!", task_context, package, type);
+		return nullptr;
+	}
+	if (!import(package)) {
+		reportLoadError("Unable to import package!", task_context, package, type);
+		return nullptr;
+	}
 
 	// And create a name for our new component.
+	// First, we need access to the Controller Manager
 	RTT::TaskContext* controllerManager = deployer->getPeer("atrias_cm");
+	if (!controllerManager) {
+		reportLoadError("Could not access the Controller Manager!", task_context, package, type);
+		return nullptr;
+	}
+
+	// And now to actually get the name.
 	RTT::OperationCaller<std::string(std::string, std::string)> getUniqueName;
 	getUniqueName = controllerManager->getOperation("getUniqueName");
+	if (!getUniqueName.ready()) {
+		reportLoadError("Could not get the Controller Manager's getUniqueName Operation!", task_context, package, type);
+		return nullptr;
+	}
 	name = getUniqueName(task_context->getName(), type);
 
 	// And actually load the controller
 	RTT::OperationCaller<bool(std::string, std::string)> loadComponent;
 	loadComponent = deployer->getOperation("loadComponent");
-	loadComponent(name, type);
+	if (!loadComponent.ready()) {
+		reportLoadError("Could not get the deployer's loadComponent Operation!", task_context, package, type);
+		return nullptr;
+	}
+	if (!loadComponent(name, type)) {
+		reportLoadError("Could not load the subcontroller!", task_context, package, type);
+		return nullptr;
+	}
 
 	// We need to be able to talk to this component.
 	// Note: After construction, the deployer automatically adds
 	// it as a peer, but it's only a one-way link.
 	subcontroller = deployer->getPeer(name);
+	if (!subcontroller) {
+		reportLoadError("Could not connect with the subcontroller!", task_context, package, type);
+		return nullptr;
+	}
 
 	// Make the deployer a peer of this component.
 	// This makes the link two-way
-	subcontroller->addPeer(deployer);
+	if (!subcontroller->addPeer(deployer)) {
+		reportLoadError("Could not add the deployer as a peer of the subcontroller!", task_context, package, type);
+		return nullptr;
+	}
 
 	// Configure it.
 	RTT::OperationCaller<bool(void)> configureController;
 	configureController = subcontroller->getOperation("configure");
-	configureController();
+	if (!configureController.ready()) {
+		reportLoadError("Unable to get subcontroller's configure operation!", task_context, package, type);
+		return nullptr;
+	}
+	if (!configureController()) {
+		reportLoadError("Subcontroller configuration failed!", task_context, package, type);
+		return nullptr;
+	}
 
 	// And start it.
 	RTT::OperationCaller<bool(void)> startController;
 	startController = subcontroller->getOperation("start");
-	startController();
+	if (!startController.ready()) {
+		reportLoadError("Unable to get the subcontroller's start operation!", task_context, package, type);
+		return nullptr;
+	}
+	if (!startController()) {
+		reportLoadError("Subcontroller starting failed!", task_context, package, type);
+		return nullptr;
+	}
 
 	return subcontroller;
 }
@@ -57,17 +127,52 @@ ASCLoader::~ASCLoader() {
 	// Stop it
 	RTT::OperationCaller<bool(void)> stopController;
 	stopController = subcontroller->getOperation("stop");
-	stopController();
+	if (!stopController.ready()) {
+		reportUnloadError("Could not acquire stop operation!");
+	} else if (!stopController()) {
+		reportUnloadError("Failed to stop subcontroller!");
+	}
 
 	// And tell it to clean up.
 	RTT::OperationCaller<bool(void)> cleanupController;
 	cleanupController = subcontroller->getOperation("cleanup");
-	cleanupController();
+	if (!cleanupController.ready()) {
+		reportUnloadError("Unable to acquire cleanup operation!");
+	} else if (!cleanupController()) {
+		reportUnloadError("Unable to cleanup subcontroller!");
+	}
 
 	// Then unload it.
 	RTT::OperationCaller<bool(std::string)> unloadComponent;
 	unloadComponent = deployer->getOperation("unloadComponent");
-	unloadComponent(name);
+	if (!unloadComponent.ready()) {
+		reportUnloadError("Unable to acquire deployer's unloadComponent operation!");
+	} else if (!unloadComponent(name)) {
+		reportUnloadError("Unable to unload the component!");
+	}
+}
+
+void ASCLoader::reportLoadError(std::string       message,
+                                RTT::TaskContext* task_context,
+                                std::string       package,
+                                std::string       type)
+{
+	log(RTT::Error) << "[ASCLoader] " << message;
+
+	// Let them know what controller this is happening in
+	if (task_context) {
+		log(RTT::Error) << " Calling component: " << task_context->getName();
+	}
+
+	// More info to help them:
+	log(RTT::Error) << " Requested component: \"" << type
+	                << "\" from package: \"" << package << "\"" << RTT::endlog();
+}
+
+void ASCLoader::reportUnloadError(std::string message) {
+	log(RTT::Error) << "[ASCLoader] " << message
+	                << " Subcontroller name: "
+	                << subcontroller->getName() << RTT::endlog();
 }
 
 }
