4eeeb45c13f37e1351a763c76581044959a238a5
Ryan Van Why <jrvanwhy@gmail.com>
2012-12-11 17:00:24 -0800

    Work on the GUI for atc_force_hopping

--- software/atrias_controllers/atc_force_hopping/include/atc_force_hopping/controller_gui.h
+++ software/atrias_controllers/atc_force_hopping/include/atc_force_hopping/controller_gui.h
@@ -24,21 +24,22 @@ atc_force_hopping::controller_input controllerDataOut;
 atc_force_hopping::controller_status controllerDataIn;
 
 // GUI elements
-Gtk::HScale *torque_A_hscale,
-        *torque_B_hscale,
-        *torque_hip_hscale;
+Gtk::HScale     *flightLegLen,
+                *retractDiff;
 
-Gtk::CheckButton *set_position_checkbutton;
+Gtk::SpinButton *flightP,
+                *flightD,
+                *stanceP,
+                *stanceD;
 
-// Parameters
-//     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
-//     arise when both C and python nodes access the same parameter. See:
-//     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
-double torque_A_param;
-double torque_B_param;
-double torque_hip_param;
+Gtk::ComboBox   *stancePSrc,
+                *stanceDSrc,
+                *legMode;
+
+Gtk::Button     *shutdownBtn;
 
 void controllerCallback(const atc_force_hopping::controller_status &status);
 
 #endif /* CONTROLLER_GUI_H_ */
 
+// vim: noexpandtab
--- software/atrias_controllers/atc_force_hopping/src/controller_gui.cpp
+++ software/atrias_controllers/atc_force_hopping/src/controller_gui.cpp
@@ -11,58 +11,76 @@
 
 //! \brief Initialize the GUI.
 bool guiInit(Glib::RefPtr<Gtk::Builder> gui) {
-    gui->get_widget("torque_A_hscale", torque_A_hscale);
-    gui->get_widget("torque_B_hscale", torque_B_hscale);
-    gui->get_widget("torque_hip_hscale", torque_hip_hscale);
+	gui->get_widget("flightLegLen",    flightLegLen);
+	gui->get_widget("retractDiff",     retractDiff);
+	gui->get_widget("flightP",         flightP);
+	gui->get_widget("flightD",         flightD);
+	gui->get_widget("stanceP",         stanceP);
+	gui->get_widget("stanceD",         stanceD);
+	gui->get_widget("stancePSrcCombo", stancePSrc);
+	gui->get_widget("stanceDSrcCombo", stanceDSrc);
+	gui->get_widget("shutdownBtn",     shutdownBtn);
 
-    if (torque_A_hscale && torque_B_hscale && torque_hip_hscale) {
-        // Set ranges.
-        torque_A_hscale->set_range(-10., 10.);
-        torque_B_hscale->set_range(-10., 10.);
-        torque_hip_hscale->set_range(-10., 10.);
+	if (!flightLegLen ||
+	    !retractDiff  ||
+	    !flightP      ||
+	    !stanceD      ||
+	    !flightP      ||
+	    !stanceD      ||
+	    !stancePSrc   ||
+	    !stanceDSrc   ||
+	    !shutdownBtn) {
+	
+		// Oops, GUI construction failure...
+		return false;
+	}
 
-        // Set up subscriber and publisher.
-        sub = nh.subscribe("atc_force_hopping_status", 0, controllerCallback);
-        pub = nh.advertise<atc_force_hopping::controller_input>("atc_force_hopping_input", 0);
-        return true;
-    }
-    return false;
+	// Set ranges.
+	torque_A_hscale->set_range(-10., 10.);
+	torque_B_hscale->set_range(-10., 10.);
+	torque_hip_hscale->set_range(-10., 10.);
+
+	// Set up subscriber and publisher.
+	sub = nh.subscribe("atc_force_hopping_status", 0, controllerCallback);
+	pub = nh.advertise<atc_force_hopping::controller_input>("atc_force_hopping_input", 0);
+	return true;
 }
 
 //! \brief Update our local copy of the controller status.
 void controllerCallback(const atc_force_hopping::controller_status &status) {
-    controllerDataIn = status;
+	controllerDataIn = status;
 }
 
 //! \brief Get parameters from the server and configure GUI accordingly.
 void getParameters() {
-    // Get parameters in the atrias_gui namespace.
-    nh.getParam("/atrias_gui/torque_A", torque_A_param);
-    nh.getParam("/atrias_gui/torque_B", torque_B_param);
-    nh.getParam("/atrias_gui/torque_hip", torque_hip_param);
+	// Get parameters in the atrias_gui namespace.
+	nh.getParam("/atrias_gui/torque_A", torque_A_param);
+	nh.getParam("/atrias_gui/torque_B", torque_B_param);
+	nh.getParam("/atrias_gui/torque_hip", torque_hip_param);
 
-    // Configure the GUI.
-    torque_A_hscale->set_value(torque_A_param);
-    torque_B_hscale->set_value(torque_B_param);
-    torque_hip_hscale->set_value(torque_hip_param);
+	// Configure the GUI.
+	torque_A_hscale->set_value(torque_A_param);
+	torque_B_hscale->set_value(torque_B_param);
+	torque_hip_hscale->set_value(torque_hip_param);
 }
 
 //! \brief Set parameters on server according to current GUI settings.
 void setParameters() {
-    nh.setParam("/atrias_gui/torque_A", torque_A_param);
-    nh.setParam("/atrias_gui/torque_B", torque_B_param);
-    nh.setParam("/atrias_gui/torque_hip", torque_hip_param);
+	nh.setParam("/atrias_gui/torque_A", torque_A_param);
+	nh.setParam("/atrias_gui/torque_B", torque_B_param);
+	nh.setParam("/atrias_gui/torque_hip", torque_hip_param);
 }
 
 //! \brief Update the GUI.
 void guiUpdate() {
-    controllerDataOut.des_motor_torque_A   = torque_A_param   = torque_A_hscale->get_value();
-    controllerDataOut.des_motor_torque_B   = torque_B_param   = torque_B_hscale->get_value();
-    controllerDataOut.des_motor_torque_hip = torque_hip_param = torque_hip_hscale->get_value();
-    pub.publish(controllerDataOut);
+	controllerDataOut.des_motor_torque_A   = torque_A_param   = torque_A_hscale->get_value();
+	controllerDataOut.des_motor_torque_B   = torque_B_param   = torque_B_hscale->get_value();
+	controllerDataOut.des_motor_torque_hip = torque_hip_param = torque_hip_hscale->get_value();
+	pub.publish(controllerDataOut);
 }
 
 //! \brief Take down the GUI.
 void guiTakedown() {
 }
 
+// vim: noexpandtab
