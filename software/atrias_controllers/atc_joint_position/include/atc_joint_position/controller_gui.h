46c0e7361a0b92c155649d60a1c4e9175241e79e
Soo-Hyun Yoo <yoos117@gmail.com>
2013-01-25 14:36:58 -0800

    Add element pointers.

--- software/atrias_controllers/atc_joint_position/include/atc_joint_position/controller_gui.h
+++ software/atrias_controllers/atc_joint_position/include/atc_joint_position/controller_gui.h
@@ -24,19 +24,28 @@ atc_joint_position::controller_input controllerDataOut;
 atc_joint_position::controller_status controllerDataIn;
 
 // GUI elements
-Gtk::HScale *torque_A_hscale,
-        *torque_B_hscale,
-        *torque_hip_hscale;
+Gtk::SpinButton *p_hl_spinbutton,
+                *d_hl_spinbutton,
+                *a_hl_spinbutton,
+                *p_hr_spinbutton,
+                *d_hr_spinbutton,
+                *a_hr_spinbutton,
+                *p_ll_spinbutton,
+                *d_ll_spinbutton,
+                *a_ll_spinbutton,
+                *l_ll_spinbutton,
+                *p_lr_spinbutton,
+                *d_lr_spinbutton,
+                *a_lr_spinbutton,
+                *l_lr_spinbutton;
 
-Gtk::CheckButton *set_position_checkbutton;
+Gtk::CheckButton *vertical_checkbutton,
+                 *sync_checkbutton;
 
 // Parameters
 //     NOTE: ROS parameters cannot be floats so loss-of-precision issues do not
 //     arise when both C and python nodes access the same parameter. See:
 //     http://answers.ros.org/question/10938/why-cant-you-use-floats-for-accessing-parameters-in-roscpp/
-double torque_A_param;
-double torque_B_param;
-double torque_hip_param;
 
 void controllerCallback(const atc_joint_position::controller_status &status);
 
