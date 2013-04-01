d37da12de966b332461462ff8dab600489ef9676
Ryan Van Why <jrvanwhy@gmail.com>
2013-04-01 10:18:22 -0700

    More work on the controller library.

--- software/atrias_control_lib/include/atrias_control_lib/ATC.hpp
+++ software/atrias_control_lib/include/atrias_control_lib/ATC.hpp
@@ -12,11 +12,18 @@
 #include <cstddef> // nullptr_t
 
 // Orocos includes
+#include <rtt/InputPort.hpp>   // So we can receive data.
 #include <rtt/TaskContext.hpp> // We're a component aka TaskContext
 
 // Robot state and controller output
 #include <atrias_msgs/controller_output.h>
 #include <atrias_msgs/robot_state.h>
+
+// This lets us check whether to create/check ports
+#include <atrias_shared/notNullPtr.hpp>
+
+// For the medulla state enum
+#include <../../robot_definitions/robot_invariant_defs.h>
 
 // We subclass this, so let's include it
 #include "atrias_control_lib/AtriasController.hpp"
@@ -28,7 +35,7 @@ namespace controller {
 // This is a component, so we subclass TaskContext;
 // as a controller, this subclasses AtriasController
 // Also, we're a template...
-template <typename logType, typename guiInType, typename guiOutType>
+template <typename logType = std::nullptr_t, typename guiInType = std::nullptr_t, typename guiOutType = std::nullptr_t>
 class ATC : public RTT::TaskContext, public AtriasController {
 	public:
 		/**
@@ -37,12 +44,7 @@ class ATC : public RTT::TaskContext, public AtriasController {
 		  * The name should be passed directly -- it's not the controller's
 		  * choice.
 		  */
-		ATC(const std::string &name) :
-			RTT::TaskContext(name),
-			AtriasController(name)
-		{
-			// The magic is done above
-		}
+		ATC(const std::string &name);
 
 	protected:
 		// These member variables should be set/read from by
@@ -68,10 +70,57 @@ class ATC : public RTT::TaskContext, public AtriasController {
 		  * @brief This allows subcontrollers to access the TaskContext
 		  * @return A reference to the TaskContext.
 		  */
-		RTT::TaskContext& getTaskContext() const {
-			return *((RTT::TaskContext*) this);
-		}
+		RTT::TaskContext& getTaskContext() const;
+
+		// Port for input data from the GUI
+		RTT::InputPort<guiInType> guiInPort;
+
+		/**
+		  * @brief This callback is executed when data is received from the GUI
+		  */
+		void guiInCallback(RTT::base::PortInterface* portInterface);
+
+		/**
+		  * @brief This is the operation called cyclically by RT Ops
+		  * This runs the controller.
+		  * @param robotState The new robot state
+		  * @return           The new controller output.
+		  */
+		atrias_msgs::controller_output& runController(atrias_msgs::robot_state& robotState);
 };
+
+template <typename logType, typename guiInType, typename guiOutType>
+ATC<logType, guiInType, guiOutType>::ATC(const std::string &name) :
+	RTT::TaskContext(name),
+	AtriasController(name)
+{
+	// Register the operation runController()
+	this->provides("atc")
+		->addOperation("runController", &ATC<logType, guiInType, guiOutType>::runController, this, RTT::ClientThread)
+		.doc("Run the controller. Takes in the robot state and returns a controller output.");
+
+	// Set up the event port for incoming GUI data (if there is incoming GUI data)
+	if (atrias::shared::notNullPtr<guiInType>())
+		this->addEventPort("Gui Input", guiInPort, boost::bind(&ATC<logType, guiInType, guiOutType>::guiInCallback, this, _1));
+}
+
+template <typename logType, typename guiInType, typename guiOutType>
+RTT::TaskContext& ATC<logType, guiInType, guiOutType>::getTaskContext() const {
+	return *((RTT::TaskContext*) this);
+}
+
+template <typename logType, typename guiInType, typename guiOutType>
+void ATC<logType, guiInType, guiOutType>::guiInCallback(RTT::base::PortInterface* portInterface) {
+	this->guiInPort.read(this->guiIn);
+}
+
+template <typename logType, typename guiInType, typename guiOutType>
+atrias_msgs::controller_output& ATC<logType, guiInType, guiOutType>::runController(atrias_msgs::robot_state& robotState) {
+	this->rs = robotState;
+	this->controller();
+	this->co.command = medulla_state_run;
+	return this->co;
+}
 
 }
 }
