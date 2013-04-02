72ec9737d0e131a024468e457645fc2193a7abf8
Andrew Peekema <apeekema@gmail.com>
2013-04-02 16:15:45 -0700

    Cut down the template controller to what will be needed for SLIP walking

--- software/atrias_controllers/atc_slip_walking/include/atc_slip_walking/ATCSlipWalking.hpp
+++ software/atrias_controllers/atc_slip_walking/include/atc_slip_walking/ATCSlipWalking.hpp
@@ -23,7 +23,6 @@
 
 // Our subcontroller types
 #include <asc_pd/ASCPD.hpp>
-#include <asc_rate_limit/ASCRateLimit.hpp>
 
 // Namespaces we're using
 using namespace std;
@@ -33,48 +32,16 @@ using namespace atc_slip_walking;
 namespace atrias {
 namespace controller {
 
-/* Our class definition. We subclass ATC for a top-level controller.
- * If we don't need a data type (such as the controller-to-gui message),
- * we simply leave that spot in the template blank. The following example
- * shows the necessary definition if this controller were not to transmit
- * data to the GUI:
- *     class ATCSlipWalking : public ATC<log_data, gui_to_controller,>
- *
- * Here, we don't need any log data, but we do communicate both ways w/ the GUI
- */
 class ATCSlipWalking : public ATC<atc_slip_walking::controller_log_data, controller_input, controller_status> {
     public:
-        /**
-          * @brief The constructor for this controller.
-          * @param name The name of this component.
-          * Every top-level controller will have this name parameter,
-          * just like current controllers.
-          */
         ATCSlipWalking(string name);
-    
+
     private:
-        /**
-          * @brief This is the main function for the top-level controller.
-          * The ATC class automatically handles startup and shutdown,
-          * if they are not disabled.
-          */
         void controller();
 
-        // PD controllers for each motor
-        ASCPD pdLA; // Left  A
-        ASCPD pdLB; // Left  B
-        ASCPD pdLH; // Left  Hip
-        ASCPD pdRA; // Right A
-        ASCPD pdRB; // Right B
-        ASCPD pdRH; // Right Hip
-
-        // Rate limiters for each motor
-        ASCRateLimit rateLimLA; // Left  A
-        ASCRateLimit rateLimLB; // Left  B
-        ASCRateLimit rateLimLH; // Left  Hip
-        ASCRateLimit rateLimRA; // Right A
-        ASCRateLimit rateLimRB; // Right B
-        ASCRateLimit rateLimRH; // Right Hip
+        // PD controllers
+        ASCPD pdLeg;
+        ASCPD pdHip;
 };
 
 }
--- software/atrias_controllers/atc_slip_walking/src/ATCSlipWalking.cpp
+++ software/atrias_controllers/atc_slip_walking/src/ATCSlipWalking.cpp
@@ -1,82 +1,48 @@
 #include "atc_slip_walking/ATCSlipWalking.hpp"
 
-// The namespaces this controller resides in
 namespace atrias {
 namespace controller {
 
-// This constructor call is much simpler.
 ATCSlipWalking::ATCSlipWalking(string name) :
     ATC(name),
-    pdLA(this, "pdLA"),
-    pdLB(this, "pdLB"),
-    pdLH(this, "pdLHip"),
-    pdRA(this, "pdRA"),
-    pdRB(this, "pdRB"),
-    pdRH(this, "pdRHip"),
-    rateLimLA(this, "rateLimLA"),
-    rateLimLB(this, "rateLimLB"),
-    rateLimLH(this, "rateLimLHip"),
-    rateLimRA(this, "rateLimRA"),
-    rateLimRB(this, "rateLimRB"),
-    rateLimRH(this, "rateLimRHip")
+    pdLeg(this, "pdLeg"),
+    pdHip(this, "pdHip")
 {
-    // We don't need to do much here, just call the ATC() constructor above.
+    // Nothing is needed
 }
 
 void ATCSlipWalking::controller() {
-    // The robot state is in an inherited member variable named rs
-    // The controller output should be put in the inherited member co
+    // Notation
+    // Robot state: rs
+    // Controller output: co
 
-    // The maximum rate of motion
-    double rate = 1.0;
+    // Do nothing if disabled
+    if (!isEnabled())
+        return;
 
-    // Reset the rate limiters if we're disabled. If not, disabling,
-    // moving the robot or the setpoints, then enabling produces
-    // a jump.
-    if (!isEnabled()) {
-        rateLimLA.reset(rs.lLeg.halfA.rotorAngle);
-        rateLimLB.reset(rs.lLeg.halfB.rotorAngle);
-        rateLimLH.reset(rs.lLeg.hip.legBodyAngle);
-        rateLimRA.reset(rs.rLeg.halfA.rotorAngle);
-        rateLimRB.reset(rs.rLeg.halfB.rotorAngle);
-        rateLimRH.reset(rs.rLeg.hip.legBodyAngle);
-    }
-
-    /* The logOut class is an inherited member from ATC, and is of type
-     * controller_log_data. guiIn, similarly, is of type
-     * gui_to_controller
-     */
-    logOut.lATgt = rateLimLA(guiIn.des_motor_ang_left_A,    rate);
-    logOut.lBTgt = rateLimLB(guiIn.des_motor_ang_left_B,    rate);
-    logOut.lHTgt = rateLimLH(guiIn.des_motor_ang_left_hip,  rate);
-    logOut.rATgt = rateLimRA(guiIn.des_motor_ang_right_A,   rate);
-    logOut.rBTgt = rateLimRB(guiIn.des_motor_ang_right_B,   rate);
-    logOut.rHTgt = rateLimRH(guiIn.des_motor_ang_right_hip, rate);
+    // Startup is handled by the ATC class.  It slowly ramps up
+    // over several seconds.  Set to false to disable
+    // setStartupEnabled(true)
 
     // Set gains
     // Legs
-    pdLA.P = pdLB.P = pdRA.P = pdRB.P = guiIn.leg_motor_p_gain;
-    pdLA.D = pdLB.D = pdRA.D = pdRB.D = guiIn.leg_motor_d_gain;
+    pdLeg.P = guiIn.leg_motor_p_gain;
+    pdLeg.D = guiIn.leg_motor_d_gain;
     // Hips
-    pdLH.P = pdRH.P = guiIn.hip_motor_p_gain;
-    pdLH.D = pdRH.D = guiIn.hip_motor_d_gain;
+    pdHip.P = guiIn.hip_motor_p_gain;
+    pdHip.D = guiIn.hip_motor_d_gain;
 
     // Command the outputs (and copy to our logging data).
-    // This is where the definition of ASCPD as a functor is convenient.
     // Legs
-    co.lLeg.motorCurrentA = pdLA(logOut.lATgt, rs.lLeg.halfA.rotorAngle, 0, rs.lLeg.halfA.rotorVelocity);
-    co.lLeg.motorCurrentB = pdLB(logOut.lBTgt, rs.lLeg.halfB.rotorAngle, 0, rs.lLeg.halfB.rotorVelocity);
-    co.rLeg.motorCurrentA = pdRA(logOut.rATgt, rs.rLeg.halfA.rotorAngle, 0, rs.rLeg.halfA.rotorVelocity);
-    co.rLeg.motorCurrentB = pdRB(logOut.rBTgt, rs.rLeg.halfB.rotorAngle, 0, rs.rLeg.halfB.rotorVelocity);
+    co.lLeg.motorCurrentA = pdLeg(0, rs.lLeg.halfA.rotorAngle, 0, rs.lLeg.halfA.rotorVelocity);
+    co.lLeg.motorCurrentB = pdLeg(0, rs.lLeg.halfB.rotorAngle, 0, rs.lLeg.halfB.rotorVelocity);
+    co.rLeg.motorCurrentA = pdLeg(0, rs.rLeg.halfA.rotorAngle, 0, rs.rLeg.halfA.rotorVelocity);
+    co.rLeg.motorCurrentB = pdLeg(0, rs.rLeg.halfB.rotorAngle, 0, rs.rLeg.halfB.rotorVelocity);
     // Hips
-    co.lLeg.motorCurrentHip = pdLH(logOut.lHTgt, rs.lLeg.hip.legBodyAngle, 0, rs.lLeg.hip.legBodyVelocity);
-    co.rLeg.motorCurrentHip = pdRH(logOut.rHTgt, rs.rLeg.hip.legBodyAngle, 0, rs.rLeg.hip.legBodyVelocity);
+    co.lLeg.motorCurrentHip = pdHip(0, rs.lLeg.hip.legBodyAngle, 0, rs.lLeg.hip.legBodyVelocity);
+    co.rLeg.motorCurrentHip = pdHip(0, rs.rLeg.hip.legBodyAngle, 0, rs.rLeg.hip.legBodyVelocity);
 
-    /* Copy over positions to the GUI output data
-     * Suppose that the guiOut message has the following members:
-     *     float64 posA
-     *     float64 posB
-     */
+    // GUI Output
     guiOut.isEnabled             = isEnabled();
     guiOut.motorPositionLeftA    = rs.lLeg.halfA.rotorAngle;
     guiOut.motorPositionLeftB    = rs.lLeg.halfB.rotorAngle;
@@ -85,16 +51,11 @@ void ATCSlipWalking::controller() {
     guiOut.motorPositionLeftHip  = rs.lLeg.hip.legBodyAngle;
     guiOut.motorPositionRightHip = rs.rLeg.hip.legBodyAngle;
 
-    /* Additionally, the following functions are available to command the robot state:
-     * commandHalt();    // Trigger a Halt
-     * commandEStop();   // Trigger an EStop
-     * commandDisable(); // Disable the robot
-     * setStartupEnabled(true/false) // Enable or disable the default startup controller
-     * setShutdownEnabled(true/false) // Enable or disable the shutdown controller
-     */
+    // Logging
+    logOut.dummy = 0.0;
 }
 
-// We need to make top-level controllers components
+// The OROCOS component object
 ORO_CREATE_COMPONENT(ATCSlipWalking)
 
 }
