7f801f6ee0997470a8c4b0f808b47cbeb4f201c7
Soo-Hyun Yoo <yoos117@gmail.com>
2013-05-22 16:09:03 -0700

    Remove trailing whitespace.

--- firmware/atrias2.1/src/medulla_imu.c
+++ firmware/atrias2.1/src/medulla_imu.c
@@ -1,4 +1,4 @@
-#include "../include/medulla_imu.h" 
+#include "../include/medulla_imu.h"
 #include "../include/medulla.h"
 //--- Define ethercat PDO entries ---//
 
@@ -65,7 +65,7 @@ ecat_pdo_entry_t imu_tx_pdos[] = {{((void**)(&imu_medulla_id_pdo)),1},
 								{((void**)(&M31_pdo)),4},
 								{((void**)(&M32_pdo)),4},
 								{((void**)(&M33_pdo)),4}};
-									
+
 //ecat_pdo_entry_t imu_tx_pdos[] = {{((void**)(&M11_pdo)),4},
 								//{((void**)(&M12_pdo)),4},
 								//{((void**)(&M13_pdo)),4},
@@ -74,7 +74,7 @@ ecat_pdo_entry_t imu_tx_pdos[] = {{((void**)(&imu_medulla_id_pdo)),1},
 								//{((void**)(&M23_pdo)),4},
 								//{((void**)(&M31_pdo)),4},
 								//{((void**)(&M32_pdo)),4},
-								//{((void**)(&M33_pdo)),4}};	
+								//{((void**)(&M33_pdo)),4}};
 //
 void imu_initilize(uint8_t id, ecat_slave_t *ecat_slave, uint8_t *tx_sm_buffer, uint8_t *rx_sm_buffer, medulla_state_t **commanded_state, medulla_state_t **current_state, uint8_t **packet_counter,TC0_t *timestamp_timer, uint16_t **master_watchdog) {
 
@@ -91,7 +91,7 @@ void imu_initilize(uint8_t id, ecat_slave_t *ecat_slave, uint8_t *tx_sm_buffer, 
 }
 
 void imu_read_data(ecat_slave_t ecat_port){
-	
+
 	   // Read raw data through UART from IMU
 	   int tx_byte_cnt;
 	   int rx_byte_cnt;
@@ -101,47 +101,47 @@ void imu_read_data(ecat_slave_t ecat_port){
 	   // init UART
 	   uart_debug = uart_init_port(&PORTE, &USARTE0, uart_baud_460800, imu_tx_buffer, 10, imu_rx_buffer, 100);
 	   uart_connect_port(&uart_debug, true);
-						
+
 		// reset updata mode
 		//imu_reset_update_mode();
-		
+
 		// set to continuous mode
 		uint8_t response;
 		//response = imu_set_continuous_mode();
-		
-		// unset continuous mode	
+
+		// unset continuous mode
 		//response = imu_unset_continuous_mode();
-		
-	
+
+
 		// stop continuous mode
 		//response = imu_set_active_mode();
 
 		// soft reset
 		//imu_soft_reset();
-		
+
 	    uint32_t pResponse[67];
 		uint8_t u8_pResponse[67];
 		uint32_t *roll;
 		uint32_t *pitch;
 		uint32_t *yaw;
-		
+
 		uint8_t led_cnt = 0;
-			
+
 		while (1)
-		{	
+		{
 			// Blinking means IMU is performing fine.
 			if (led_cnt/((uint32_t)(100)))
 			{
-				LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK) | LED_GREEN;		
-			} 
+				LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK) | LED_GREEN;
+			}
 			else
 			{
-				LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK) | LED_BLUE;		
+				LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK) | LED_BLUE;
 			}
 			++led_cnt;
-			
+
 			uint32_t ptr = 0;
-					
+
 			(*imu_medulla_id_pdo) = 0;
 			(*imu_current_state_pdo)= 0;
 			(*imu_medulla_counter_pdo)= 0;
@@ -166,111 +166,111 @@ void imu_read_data(ecat_slave_t ecat_port){
 			if (1)
 			{
 				// Read data
-				//response = imu_read_accel_angRate_OriMatrix(u8_pResponse);						 				
+				//response = imu_read_accel_angRate_OriMatrix(u8_pResponse);
 				response = imu_pull_accel_angRate_OriMatrix(u8_pResponse);
-				
+
 				if (response)
 				{
 					//_delay_ms(1);
-					 
-					 
+
+
 					for (uint8_t i=0;i<67;i++)
 					{
 						pResponse[i] = u8_pResponse[i];
 					}
-					 
-					 
+
+
 						//TxPDO entries
 					(*imu_medulla_id_pdo) = 1;
 					(*imu_current_state_pdo) = 2;
 					(*imu_medulla_counter_pdo) = 3;
 					(*imu_error_flags_pdo) = 4;
-			
+
 					uint8_t *ptr;
-					
-					// XAccel	
-					ptr = u8_pResponse+1;			
+
+					// XAccel
+					ptr = u8_pResponse+1;
 					populate_byte_to_data(ptr,XAccel_pdo);
-					
+
 					// YAccel
-					ptr = u8_pResponse+5;			
+					ptr = u8_pResponse+5;
 					populate_byte_to_data(ptr,YAccel_pdo);
-					
+
 					// ZAccel
-					ptr = u8_pResponse+9;			
+					ptr = u8_pResponse+9;
 					populate_byte_to_data(ptr,ZAccel_pdo);
-					
+
 					// XAngRate
-					ptr = u8_pResponse+13;			
+					ptr = u8_pResponse+13;
 					populate_byte_to_data(ptr,XAngRate_pdo);
-					
+
 					// YAngRate
-					ptr = u8_pResponse+17;			
+					ptr = u8_pResponse+17;
 					populate_byte_to_data(ptr,YAngRate_pdo);
-					
-			
+
+
 					// ZAngRate
-					ptr = u8_pResponse+21;			
+					ptr = u8_pResponse+21;
 					populate_byte_to_data(ptr,ZAngRate_pdo);
-					
-					
+
+
 					// M11
 					byte = 25;
-					ptr = u8_pResponse+25;			
+					ptr = u8_pResponse+25;
 					populate_byte_to_data(ptr,M11_pdo);
-					
+
 					// M12
-					ptr = u8_pResponse+29;			
+					ptr = u8_pResponse+29;
 					populate_byte_to_data(ptr,M12_pdo);
-					
+
 					// M13
-					ptr = u8_pResponse+33;			
+					ptr = u8_pResponse+33;
 					populate_byte_to_data(ptr,M13_pdo);
-					
-				
+
+
 					// M21
-					ptr = u8_pResponse+37;			
+					ptr = u8_pResponse+37;
 					populate_byte_to_data(ptr,M21_pdo);
-					
-				
+
+
 					// M22
-					ptr = u8_pResponse+41;			
+					ptr = u8_pResponse+41;
 					populate_byte_to_data(ptr,M22_pdo);
-					
+
 
 					// M23
-					ptr = u8_pResponse+45;			
+					ptr = u8_pResponse+45;
 					populate_byte_to_data(ptr,M23_pdo);
-					
+
 					// M31
-					ptr = u8_pResponse+49;			
+					ptr = u8_pResponse+49;
 					populate_byte_to_data(ptr,M31_pdo);
-					
-					
+
+
 					// M32
-					ptr = u8_pResponse+53;			
+					ptr = u8_pResponse+53;
 					populate_byte_to_data(ptr,M32_pdo);
-					
+
 
 					// M33
-					ptr = u8_pResponse+57;			
+					ptr = u8_pResponse+57;
 					populate_byte_to_data(ptr,M33_pdo);
-					
+
 				//
 			}
-												
-		} 
+
+		}
 		//Read new commands from the ethercat slave
 		ecat_read_rx_sm(&ecat_port);
 		//Send the new sensor data to the ethercat slave
 		ecat_write_tx_sm(&ecat_port);
-					
+
 		//_delay_ms(15);
 	}
-			
-			
-	//_delay_ms(50);			
-				
+
+
+	//_delay_ms(50);
+
 
 }
 
@@ -279,26 +279,26 @@ bool imu_calculating_checksum(uint8_t *rx_buffer,uint8_t rx_buffer_length){
 	uint8_t tix;
 	uint16_t tChksum;
 	uint16_t tResponseChksum;
-	
+
 	tChksum = 0;
 	for (tix = 0;tix < (rx_buffer_length-2);tix++)
 	{
 		tChksum += (uint16_t)(rx_buffer[tix]);
 	}
-	
+
 	tResponseChksum = 0;
 	tResponseChksum = rx_buffer[rx_buffer_length-2] << 8;
 	tResponseChksum += rx_buffer[rx_buffer_length-1];
-	
+
 	if (tChksum==tResponseChksum)
 	{
 		tGoodResponse = true;
-	} 
+	}
 	else
 	{
 		tGoodResponse = false;
 	}
-	
+
 	return tGoodResponse;
 }
 
@@ -307,7 +307,7 @@ uint8_t imu_set_continuous_mode(void){
 		unsigned char rx_buffer[8];
 		uint8_t response;
 		uint8_t tx_buffer_length;
-		
+
 
 		while (1)
 		{
@@ -315,14 +315,14 @@ uint8_t imu_set_continuous_mode(void){
 			uart_tx_data(&uart_debug,tx_buffer, 4);
 			uart_rx_data(&uart_debug,rx_buffer,8);
 			//_delay_ms(5);
-			
+
 			if ((rx_buffer[0] == 0xC4))
 			{
 				return rx_buffer[0];
 			}
-			
+
 		}
-			
+
 }
 
 uint8_t imu_unset_continuous_mode(void){
@@ -330,7 +330,7 @@ uint8_t imu_unset_continuous_mode(void){
 		uint8_t rx_buffer[1];
 		uint8_t response;
 		uint8_t tx_buffer_length;
-		
+
 		tx_buffer[0] = 0xFA;
 		tx_buffer[1] = 0x75;
 		tx_buffer[2] = 0xB4;
@@ -340,22 +340,22 @@ uint8_t imu_unset_continuous_mode(void){
 			tx_buffer_length = 0;
 			tx_buffer_length = uart_tx_data(&uart_debug,tx_buffer, 3);
 			uart_rx_data(&uart_debug,rx_buffer,1);
-			
-			
+
+
 			//if ((rx_buffer[0] == 0xC4))
 			//{
 				return rx_buffer[0];
 			//}
-			
+
 		//}
-			
+
 }
 
 
 uint8_t imu_set_active_mode(void){
 		uint8_t tx_buffer[4];
 		uint8_t rx_buffer[4];
-		
+
 		tx_buffer[0] = 0xD4;
 		tx_buffer[1] = 0xA3;
 		tx_buffer[2] = 0x47;
@@ -366,39 +366,39 @@ uint8_t imu_set_active_mode(void){
 			uart_tx_data(&uart_debug,tx_buffer, 4);
 			uart_rx_data(&uart_debug,rx_buffer,4);
 			_delay_ms(5);
-		}			
-		
+		}
+
 }
 
 uint8_t imu_read_euler_angles(uint32_t *ptr_roll, uint32_t *ptr_pitch, uint32_t *ptr_yaw){
 	uint8_t tx_buffer[1];
 		uint8_t rx_buffer[19];
-		
+
 		tx_buffer[0] = 0xCE;
 
-					
+
 		uart_tx_data(&uart_debug,tx_buffer, 1);
 		uart_rx_data(&uart_debug,rx_buffer,19);
-		
+
 		// read euler angles
-		
+
 		(*ptr_roll) = SHIFT_3BYTE(rx_buffer[1]);
 		(*ptr_roll) += SHIFT_2BYTE(rx_buffer[2]);
 		(*ptr_roll) += SHIFT_1BYTE(rx_buffer[3]);
 		(*ptr_roll) += rx_buffer[4];
-		
+
 		(*ptr_pitch) = SHIFT_3BYTE(rx_buffer[5]);
 		(*ptr_pitch) += SHIFT_2BYTE(rx_buffer[6]);
 		(*ptr_pitch) += SHIFT_1BYTE(rx_buffer[7]);
 		(*ptr_pitch) += rx_buffer[8];
-		
+
 		(*ptr_yaw) = SHIFT_3BYTE(rx_buffer[9]);
 		(*ptr_yaw) += SHIFT_2BYTE(rx_buffer[10]);
 		(*ptr_yaw) += SHIFT_1BYTE(rx_buffer[11]);
 		(*ptr_yaw) += rx_buffer[12];
-		
-		
-		
+
+
+
 		return rx_buffer[0];
 }
 
@@ -408,30 +408,30 @@ uint8_t imu_read_accel_angRate_OriMatrix(uint8_t *ptr_response){
 		uint8_t rx_buffer[67];
 		uint8_t goodResponse;
 		tx_buffer[0] = 0x00;
-		uint8_t rx_buffer_length;	
+		uint8_t rx_buffer_length;
 
 		while (1)
 		{
 			//uart_tx_data(&uart_debug,tx_buffer, 1);
 			rx_buffer_length = uart_rx_data(&uart_debug,rx_buffer,67);
-			
+
 			// Read good package
 			//goodResponse = 0;
 			//if (rx_buffer[0]==0xC8)
 			//{
 				//goodResponse  = imu_calculating_checksum(rx_buffer,67);
 			//}
-			
+
 			// stop and return if good package detected
 			if (rx_buffer[0]==0xC8)
 			{
 				for (int i=0;i<67;i++)
 				{
-					*(ptr_response+i) = rx_buffer[i];	
-				}						
+					*(ptr_response+i) = rx_buffer[i];
+				}
 				return rx_buffer[0];
 			}
-			
+
 		}
 }
 
@@ -441,42 +441,42 @@ uint8_t imu_pull_accel_angRate_OriMatrix(uint8_t *ptr_response){
 		uint8_t tx_buffer[1];
 		uint8_t rx_buffer[67];
 		uint8_t buffer[67];
-		
+
 		int package_length = 67;
 		bool goodResponse;
-		int rx_buffer_length;	
+		int rx_buffer_length;
 		int tx_buffer_length;
-		
+
 		uint16_t cnt = 0;
 		uint16_t offset = 0;
-		
-		tx_buffer[0] = 0xC8;	
+
+		tx_buffer[0] = 0xC8;
 		while (1)
 		{
 			for (uint8_t i=0;i<67;i++)
 			{
-				buffer[i] = 0;	
+				buffer[i] = 0;
 			}
-			
+
 			// send command
 			tx_buffer_length = uart_tx_data(&uart_debug,tx_buffer,1);
-			
+
 			// grab data
 			rx_buffer_length = 0;
 			cnt = 0;
 			while (rx_buffer_length<67)
 			{
 				offset = uart_rx_data(&uart_debug,rx_buffer,67);
-							
+
 				for (int i=0;i<offset;i++)
 				{
-					buffer[cnt] = rx_buffer[i];					
+					buffer[cnt] = rx_buffer[i];
 					++cnt;
 				}
 				rx_buffer_length = rx_buffer_length + offset;
 			}
-			
-			
+
+
 			 //Read good package
 			goodResponse = 0;
 			if ((buffer[0])==200)
@@ -489,8 +489,8 @@ uint8_t imu_pull_accel_angRate_OriMatrix(uint8_t *ptr_response){
 			{
 				for (int i=0;i<67;i++)
 				{
-					*(ptr_response+i) = buffer[i];	
-				}						
+					*(ptr_response+i) = buffer[i];
+				}
 				return buffer[0];
 			}
 		}
@@ -503,17 +503,17 @@ uint8_t imu_pull_accel_angRate_OriMatrix(uint8_t *ptr_response){
 uint8_t imu_soft_reset(void){
 	uint8_t tx_buffer[3];
 	uint8_t rx_buffer[1];
-	
+
 	tx_buffer[0] = 0xFE;
 	tx_buffer[1] = 0x9E;
 	tx_buffer[2] = 0x3A;
-	
+
 
 	while (1)
 	{
 		uart_tx_data(&uart_debug,tx_buffer, 3);
 		uart_rx_data(&uart_debug,rx_buffer,1);
-		
+
 		//if (rx_buffer[0]==0xC8)
 		//{
 			//for (int i=0;i<67;i++)
@@ -522,21 +522,21 @@ uint8_t imu_soft_reset(void){
 			//}
 			//return rx_buffer[0];
 		//}
-		
+
 	}
 }
 
 uint8_t imu_reset_update_mode(void){
 	uint8_t tx_buffer[1];
 	uint8_t rx_buffer[1];
-	
+
 	tx_buffer[0] = 0x32;
 
 	while (1)
 	{
 		uart_tx_data(&uart_debug,tx_buffer, 1);
 		uart_rx_data(&uart_debug,rx_buffer,1);
-		
+
 		//if (rx_buffer[0]==0xC8)
 		//{
 			//for (int i=0;i<67;i++)
@@ -545,7 +545,7 @@ uint8_t imu_reset_update_mode(void){
 			//}
 			//return rx_buffer[0];
 		//}
-		
+
 	}
 }
 
