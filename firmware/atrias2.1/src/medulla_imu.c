8f4f1c2a6d7d16914f47fb9585ab2fa8e2dc1fe1
Soo-Hyun Yoo <yoos117@gmail.com>
2013-05-22 17:18:30 -0700

    Set PDO entries for IMU.

--- firmware/atrias2.1/src/medulla_imu.c
+++ firmware/atrias2.1/src/medulla_imu.c
@@ -1,5 +1,8 @@
 #include "../include/medulla_imu.h"
 #include "../include/medulla.h"
+
+kvh_data_t kvh_data;
+
 //--- Define ethercat PDO entries ---//
 
 #define SHIFT_1BYTE(X)	(X<<8)
@@ -13,6 +16,7 @@ uint32_t buffer[1000];
 uint32_t counter = 0;
 
 uart_port_t uart_debug;
+
 // RxPDO entries
 medulla_state_t *imu_command_state_pdo;
 uint16_t *imu_counter_pdo;
@@ -23,22 +27,15 @@ uint8_t *imu_medulla_id_pdo;
 medulla_state_t *imu_current_state_pdo;
 uint8_t *imu_medulla_counter_pdo;
 uint8_t *imu_error_flags_pdo;
-
-uint32_t *XAngRate_pdo;
+uint32_t *XAngDelta_pdo;
 uint32_t *XAccel_pdo;
-uint32_t *YAngRate_pdo;
+uint32_t *YAngDelta_pdo;
 uint32_t *YAccel_pdo;
-uint32_t *ZAngRate_pdo;
+uint32_t *ZAngDelta_pdo;
 uint32_t *ZAccel_pdo;
-uint32_t *M11_pdo;
-uint32_t *M12_pdo;
-uint32_t *M13_pdo;
-uint32_t *M21_pdo;
-uint32_t *M22_pdo;
-uint32_t *M23_pdo;
-uint32_t *M31_pdo;
-uint32_t *M32_pdo;
-uint32_t *M33_pdo;
+uint8_t  *Status_pdo;
+uint8_t  *Seq_pdo;
+int16_t  *Temp_pdo;
 
 void populate_byte_to_data(const uint8_t* data_byte,uint32_t* data);
 
@@ -50,32 +47,16 @@ ecat_pdo_entry_t imu_tx_pdos[] = {{((void**)(&imu_medulla_id_pdo)),1},
 							  {((void**)(&imu_current_state_pdo)),1},
 							  {((void**)(&imu_medulla_counter_pdo)),1},
 							  {((void**)(&imu_error_flags_pdo)),1},
-							  {((void**)(&XAngRate_pdo)),4},
+							  {((void**)(&XAngDelta_pdo)),4},
 							  {((void**)(&XAccel_pdo)),4},
-							  {((void**)(&YAngRate_pdo)),4},
+							  {((void**)(&YAngDelta_pdo)),4},
 							  {((void**)(&YAccel_pdo)),4},
-							  {((void**)(&ZAngRate_pdo)),4},
+							  {((void**)(&ZAngDelta_pdo)),4},
 							  {((void**)(&ZAccel_pdo)),4},
-								{((void**)(&M11_pdo)),4},
-								{((void**)(&M12_pdo)),4},
-								{((void**)(&M13_pdo)),4},
-								{((void**)(&M21_pdo)),4},
-								{((void**)(&M22_pdo)),4},
-								{((void**)(&M23_pdo)),4},
-								{((void**)(&M31_pdo)),4},
-								{((void**)(&M32_pdo)),4},
-								{((void**)(&M33_pdo)),4}};
+							  {((void**)(&Status_pdo)),1},
+							  {((void**)(&Seq_pdo)),1},
+							  {((void**)(&Temp_pdo)),2}};
 
-//ecat_pdo_entry_t imu_tx_pdos[] = {{((void**)(&M11_pdo)),4},
-								//{((void**)(&M12_pdo)),4},
-								//{((void**)(&M13_pdo)),4},
-								//{((void**)(&M21_pdo)),4},
-								//{((void**)(&M22_pdo)),4},
-								//{((void**)(&M23_pdo)),4},
-								//{((void**)(&M31_pdo)),4},
-								//{((void**)(&M32_pdo)),4},
-								//{((void**)(&M33_pdo)),4}};
-//
 void imu_initilize(uint8_t id, ecat_slave_t *ecat_slave, uint8_t *tx_sm_buffer, uint8_t *rx_sm_buffer, medulla_state_t **commanded_state, medulla_state_t **current_state, uint8_t **packet_counter,TC0_t *timestamp_timer, uint16_t **master_watchdog) {
 
 	ecat_init_sync_managers(ecat_slave, rx_sm_buffer, MEDULLA_IMU_OUTPUTS_SIZE, 0x1000, tx_sm_buffer, MEDULLA_IMU_INPUTS_SIZE, 0x2000);
@@ -146,21 +127,15 @@ void imu_read_data(ecat_slave_t ecat_port){
 			(*imu_current_state_pdo)= 0;
 			(*imu_medulla_counter_pdo)= 0;
 			(*imu_error_flags_pdo)= 0;
-			(*XAngRate_pdo) = 0;
+			(*XAngDelta_pdo) = 0;
 			(*XAccel_pdo)= 0;
-			(*YAngRate_pdo)= 0;
+			(*YAngDelta_pdo)= 0;
 			(*YAccel_pdo)= 0;
-			(*ZAngRate_pdo)= 0;
+			(*ZAngDelta_pdo)= 0;
 			(*ZAccel_pdo)= 0;
-			(*M11_pdo)= 0;
-			 (*M12_pdo)= 0;
-			(*M13_pdo)= 0;
-			(*M21_pdo)= 0;
-			(*M22_pdo)= 0;
-			(*M23_pdo)= 0;
-			(*M31_pdo)= 0;
-			(*M32_pdo)= 0;
-			(*M33_pdo)= 0;
+			(*Status_pdo)= 0;
+			(*Seq_pdo)= 0;
+			(*Temp_pdo)= 0;
 
 			//if (*imu_command_pdo)
 			if (1)
@@ -186,77 +161,47 @@ void imu_read_data(ecat_slave_t ecat_port){
 					(*imu_medulla_counter_pdo) = 3;
 					(*imu_error_flags_pdo) = 4;
 
+					// Populate data from IMU. Refer to p. 10 in manual for
+					// data locations.
 					uint8_t *ptr;
 
+					// XAngDelta
+					ptr = u8_pResponse+4;
+					populate_byte_to_data(ptr,XAngDelta_pdo);
+
+					// YAngDelta
+					ptr = u8_pResponse+8;
+					populate_byte_to_data(ptr,YAngDelta_pdo);
+
+
+					// ZAngDelta
+					ptr = u8_pResponse+12;
+					populate_byte_to_data(ptr,ZAngDelta_pdo);
+
 					// XAccel
-					ptr = u8_pResponse+1;
+					ptr = u8_pResponse+16;
 					populate_byte_to_data(ptr,XAccel_pdo);
 
 					// YAccel
-					ptr = u8_pResponse+5;
+					ptr = u8_pResponse+20;
 					populate_byte_to_data(ptr,YAccel_pdo);
 
 					// ZAccel
-					ptr = u8_pResponse+9;
+					ptr = u8_pResponse+24;
 					populate_byte_to_data(ptr,ZAccel_pdo);
 
-					// XAngRate
-					ptr = u8_pResponse+13;
-					populate_byte_to_data(ptr,XAngRate_pdo);
+					// Status
+					ptr = u8_pResponse+28;
+					populate_byte_to_data(ptr,Status_pdo);
 
-					// YAngRate
-					ptr = u8_pResponse+17;
-					populate_byte_to_data(ptr,YAngRate_pdo);
-
-
-					// ZAngRate
-					ptr = u8_pResponse+21;
-					populate_byte_to_data(ptr,ZAngRate_pdo);
-
-
-					// M11
-					byte = 25;
-					ptr = u8_pResponse+25;
-					populate_byte_to_data(ptr,M11_pdo);
-
-					// M12
+					// Seq
 					ptr = u8_pResponse+29;
-					populate_byte_to_data(ptr,M12_pdo);
+					populate_byte_to_data(ptr,Seq_pdo);
 
-					// M13
-					ptr = u8_pResponse+33;
-					populate_byte_to_data(ptr,M13_pdo);
-
-
-					// M21
-					ptr = u8_pResponse+37;
-					populate_byte_to_data(ptr,M21_pdo);
-
-
-					// M22
-					ptr = u8_pResponse+41;
-					populate_byte_to_data(ptr,M22_pdo);
-
-
-					// M23
-					ptr = u8_pResponse+45;
-					populate_byte_to_data(ptr,M23_pdo);
-
-					// M31
-					ptr = u8_pResponse+49;
-					populate_byte_to_data(ptr,M31_pdo);
-
-
-					// M32
-					ptr = u8_pResponse+53;
-					populate_byte_to_data(ptr,M32_pdo);
-
-
-					// M33
-					ptr = u8_pResponse+57;
-					populate_byte_to_data(ptr,M33_pdo);
-
-				//
+					// Temp
+					ptr = u8_pResponse+30;
+					populate_byte_to_data(ptr,Temp_pdo);
+				}
 			}
 
 		}
