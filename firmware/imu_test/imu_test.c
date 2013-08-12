1276e8b17b4032c63bdc62de359945c582f5dc85
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-11 20:51:16 -0700

    Change some things around to get 800 Hz UART from Medulla while reading from IMU.

--- firmware/imu_test/imu_test.c
+++ firmware/imu_test/imu_test.c
@@ -22,30 +22,32 @@ ECAT_USES_PORT(SPIE);
 #endif
 
 // Interrupt for handling watchdog (we don't need a driver for this)
-ISR(TCE1_OVF_vect) {
-	WATCHDOG_TIMER.INTCTRLA = TC_OVFINTLVL_OFF_gc;
-	estop();
-	LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK);
-	printf("[ERROR] Watchdog timer overflow\n");
-	while(1);
-}
+//ISR(TCE1_OVF_vect) {
+//	WATCHDOG_TIMER.INTCTRLA = TC_OVFINTLVL_OFF_gc;
+//	estop();
+//	LED_PORT.OUT = (LED_PORT.OUT & ~LED_MASK);
+//	printf("[ERROR] Watchdog timer overflow\n");
+//	while(1);
+//}
 
 // Limit Switches
-LIMIT_SW_USES_PORT(PORTK)
-LIMIT_SW_USES_COUNTER(TCF0)
+//LIMIT_SW_USES_PORT(PORTK)
+//LIMIT_SW_USES_COUNTER(TCF0)
 
 // BISS and SSI encoders use the SPI ports
-SPI_USES_PORT(SPIC)
-SPI_USES_PORT(SPID)
-SPI_USES_PORT(SPIF)
+//SPI_USES_PORT(SPIC)
+//SPI_USES_PORT(SPID)
+//SPI_USES_PORT(SPIF)
 
 // Amplifier on port D0
-UART_USES_PORT(USARTD0)
+//UART_USES_PORT(USARTD0)
 
 // ADCs on port a and b
-ADC_USES_PORT(ADCA)
-ADC_USES_PORT(ADCB)
+//ADC_USES_PORT(ADCA)
+//ADC_USES_PORT(ADCB)
 
+
+io_pin_t debug_pin;
 
 int main(void) {
 	// Initilize the clock to 32 Mhz oscillator
@@ -54,7 +56,8 @@ int main(void) {
 		PORTC.OUTSET = 1;
 	}
 
-	// Configure and enable all the interrupts
+	// Configure and enable all the interrupts   NOTE: Using external
+	// interrupts may mess up timing!
 	cpu_configure_interrupt_level(cpu_interrupt_level_medium, true);
 	cpu_configure_interrupt_level(cpu_interrupt_level_high, true);
 	cpu_configure_interrupt_level(cpu_interrupt_level_low, true);
@@ -66,8 +69,9 @@ int main(void) {
 	uint8_t debuginbuffer[128];
 	uint8_t imu_outbuffer[128];
 	uint8_t imu_inbuffer[128];
+	uint8_t bytes_received = 0;
 
-	debug_port = uart_init_port(&PORTE, &USARTE0, uart_baud_115200, unused_outbuffer, 128, unused_inbuffer, 128);
+	debug_port = uart_init_port(&PORTE, &USARTE0, uart_baud_460800, unused_outbuffer, 128, unused_inbuffer, 128);
 	uart_connect_port(&debug_port, true);
 
 	imu_port = uart_init_port(&PORTF, &USARTF0, uart_baud_921600, unused_outbuffer, 128, unused_inbuffer, 128);
@@ -76,20 +80,29 @@ int main(void) {
 	msync_pin = io_init_pin(&PORTF, 1);   // Initialize master sync pin for IMU.
 	PORTF.DIR = PORTF.DIR | (1<<1);   // msync pin is output.
 
+	debug_pin = io_init_pin(&PORTK, 1);   // Initialize debug pin.
+	PORTK.DIR = PORTK.DIR | (1<<1);   // Debug pin
+
+	_delay_ms(1800);   // Wait for IMU to be ready.
+
 	while (1) {
 		PORTF.OUT = PORTF.OUT | (1<<1);   // Pull msync pin high.
 		_delay_us(30);   // ..for at least 30 us.
 		PORTF.OUT = PORTF.OUT & ~(1<<1);   // Pull msync pin low.
 
-		_delay_us(100);   // Wait for IMU to finish sending data. Scope shows about 60 us delay from rising edge of msync to first bit of packet.
+		//_delay_us(1);   // Wait for IMU to finish sending data. Scope shows about 60 us delay from rising edge of msync to first bit of packet. NOTE: This doesn't work as intended when external interrupts are on, since UART will interrupt the delay!
+		// It takes 390.6 us for the IMU to send 36 bytes at 921600 baud.
 
-		uint8_t bytes_received = uart_received_bytes(&imu_port);   // DEBUG
-		uart_rx_data(&imu_port, imu_inbuffer, uart_received_bytes(&imu_port));
+		bytes_received = uart_received_bytes(&imu_port);
+		uart_rx_data(&imu_port, imu_inbuffer, bytes_received);   // This takes 200 us.
 
-		int a=28;
-		printf("[Medulla IMU] %u %x %x %x %x\n", bytes_received, imu_inbuffer[a+0], imu_inbuffer[a+1], *(imu_inbuffer+a+2), *(imu_inbuffer+a+3));
+		int a=4;
 
-		_delay_ms(2);
+		printf("%c%c%c%c%c%c%c%c%c%c%c%c\n",
+				imu_inbuffer[a+0], imu_inbuffer[a+1], imu_inbuffer[a+2], imu_inbuffer[a+3],
+				imu_inbuffer[a+4], imu_inbuffer[a+5], imu_inbuffer[a+6], imu_inbuffer[a+7],
+				imu_inbuffer[a+8], imu_inbuffer[a+9], imu_inbuffer[a+10], imu_inbuffer[a+11]);
+		_delay_us(189);   // Use the scope to determine this number for 800 Hz operation. Yeah it's a hack.
 	}
 
 	while(1);
