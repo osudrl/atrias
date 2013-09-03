823619f5d92f86b33e4ff29078c99c76c2091be6
Soo-Hyun Yoo <yoos117@gmail.com>
2013-09-02 20:37:36 -0700

    No need for a main function..

--- software/atrias/scripts/kvh_ahrs_test.py
+++ software/atrias/scripts/kvh_ahrs_test.py
@@ -19,81 +19,80 @@ elif debugType == 'direct':
     packetSepChar = '\xfe\x81\xff\x55'
 
 
-if __name__ == "__main__":
-    # Initialize serial connection.
-    try:
-        ser = serial.Serial(serialPort, baudrate, timeout=0)
-    except serial.SerialException:
-        print "Unable to open specified serial port! Exiting..."
-        exit(1)
+# Initialize serial connection.
+try:
+    ser = serial.Serial(serialPort, baudrate, timeout=0)
+except serial.SerialException:
+    print "Unable to open specified serial port! Exiting..."
+    exit(1)
 
-    loopCount   = 0
-    packetCount = 0
-    errorCount  = 0   # Count number of bad packets
-    l = ['']   # List of queued packets
-    p = ''     # Packet to parse
-    haveNewPacket = False   # Do we have a new packet (to consider printing?)
-    DT_IMU  = 0.001   # Delta T of IMU in seconds
-    DT_READ = 0.0008   # Delta T of serial read loop in seconds
+loopCount   = 0
+packetCount = 0
+errorCount  = 0   # Count number of bad packets
+l = ['']   # List of queued packets
+p = ''     # Packet to parse
+haveNewPacket = False   # Do we have a new packet (to consider printing?)
+DT_IMU  = 0.001   # Delta T of IMU in seconds
+DT_READ = 0.0008   # Delta T of serial read loop in seconds
 
-    # Absolute angle
-    x = 0.0
-    y = 0.0
-    z = 0.0
+# Absolute angle
+x = 0.0
+y = 0.0
+z = 0.0
 
-    # Change in angle
-    dx = 0.0
-    dy = 0.0
-    dz = 0.0
+# Change in angle
+dx = 0.0
+dy = 0.0
+dz = 0.0
 
-    nextLoopTime = time()   # Don't use clock(), because it's inaccurate.
-    while True:
-        if time() >= nextLoopTime:
-            nextLoopTime += DT_READ
+nextLoopTime = time()   # Don't use clock(), because it's inaccurate.
+while True:
+    if time() >= nextLoopTime:
+        nextLoopTime += DT_READ
 
-            d = ser.readline()   # Raw data
-            l = l[:-1] + (l[-1] + d).split(packetSepChar)   # Get packets. The last element in l may not necessarily be a whole packet.
+        d = ser.readline()   # Raw data
+        l = l[:-1] + (l[-1] + d).split(packetSepChar)   # Get packets. The last element in l may not necessarily be a whole packet.
 
-            # If we have at least one whole packet
-            if len(l) > 1:
-                haveNewPacket = True
-                try:
-                    p = l[0]
-                    l = l[1:]   # Pop off packet that was just read.
+        # If we have at least one whole packet
+        if len(l) > 1:
+            haveNewPacket = True
+            try:
+                p = l[0]
+                l = l[1:]   # Pop off packet that was just read.
 
-                    # Parse gyro data as big-endian floats.
-                    if debugType == 'medulla' and len(p) == 24:
-                        if imuMode == 'rate':
-                            dx = struct.unpack('>f',   p[:8].decode('hex'))[0] * 180/pi * DT_IMU
-                            dy = struct.unpack('>f', p[8:16].decode('hex'))[0] * 180/pi * DT_IMU
-                            dz = struct.unpack('>f',  p[16:].decode('hex'))[0] * 180/pi * DT_IMU
-                    elif debugType == 'direct' and len(p) == 32:
-                        if imuMode == 'rate':
-                            dx = struct.unpack('>f',   p[:4])[0] * 180/pi * DT_IMU
-                            dy = struct.unpack('>f',  p[4:8])[0] * 180/pi * DT_IMU
-                            dz = struct.unpack('>f', p[8:12])[0] * 180/pi * DT_IMU
-                        elif imuMode == 'delta':
-                            dx = struct.unpack('>f',   p[:4])[0] * 180/pi
-                            dy = struct.unpack('>f',  p[4:8])[0] * 180/pi
-                            dz = struct.unpack('>f', p[8:12])[0] * 180/pi
-                    else:
-                        errorCount += 1
-                except:
-                    pass   # Sometimes we'll mangle the first packet. Ignore this.
+                # Parse gyro data as big-endian floats.
+                if debugType == 'medulla' and len(p) == 24:
+                    if imuMode == 'rate':
+                        dx = struct.unpack('>f',   p[:8].decode('hex'))[0] * 180/pi * DT_IMU
+                        dy = struct.unpack('>f', p[8:16].decode('hex'))[0] * 180/pi * DT_IMU
+                        dz = struct.unpack('>f',  p[16:].decode('hex'))[0] * 180/pi * DT_IMU
+                elif debugType == 'direct' and len(p) == 32:
+                    if imuMode == 'rate':
+                        dx = struct.unpack('>f',   p[:4])[0] * 180/pi * DT_IMU
+                        dy = struct.unpack('>f',  p[4:8])[0] * 180/pi * DT_IMU
+                        dz = struct.unpack('>f', p[8:12])[0] * 180/pi * DT_IMU
+                    elif imuMode == 'delta':
+                        dx = struct.unpack('>f',   p[:4])[0] * 180/pi
+                        dy = struct.unpack('>f',  p[4:8])[0] * 180/pi
+                        dz = struct.unpack('>f', p[8:12])[0] * 180/pi
+                else:
+                    errorCount += 1
+            except:
+                pass   # Sometimes we'll mangle the first packet. Ignore this.
 
-                x += dx   # Integrate
-                y += dy   # Integrate
-                z += dz   # Integrate
+            x += dx   # Integrate
+            y += dy   # Integrate
+            z += dz   # Integrate
 
-                packetCount = packetCount+1
+            packetCount = packetCount+1
 
-            else:
-                haveNewPacket = False
+        else:
+            haveNewPacket = False
 
-            loopCount = loopCount+1
+        loopCount = loopCount+1
 
-            if haveNewPacket:
-                print "%2i %8i %8i   %5s (rad): %10f %10f %10f   angle (deg): %10f %10f %10f" % (errorCount, loopCount, packetCount, imuMode, dx, dy, dz, x, y, z)
+        if haveNewPacket:
+            print "%2i %8i %8i   %5s (rad): %10f %10f %10f   angle (deg): %10f %10f %10f" % (errorCount, loopCount, packetCount, imuMode, dx, dy, dz, x, y, z)
 
 
 # vim: expandtab
