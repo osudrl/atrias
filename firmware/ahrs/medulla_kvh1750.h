0708833cc2207faaa476af0fa1030f513bd8ab0a
Soo-Hyun Yoo <yoos117@gmail.com>
2013-09-08 18:25:35 -0700

    Declare print functions in header.

--- firmware/ahrs/medulla_kvh1750.h
+++ firmware/ahrs/medulla_kvh1750.h
@@ -3,11 +3,10 @@
 
 #include <medulla.h>
 
-// The DCM
-float dcm_out[3][3];
-
 void setup_kvh(void);
 void read_kvh(float gyr[3], float acc[3]);
+void print_imu();
+void print_dcm(float dcm[3][3]);
 
 #endif /* MEDULLA_KVH1750_H */
 
