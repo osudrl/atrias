68c84010a49831a044bf0ad0111b20d027b94b16
Soo-Hyun Yoo <yoos117@gmail.com>
2014-07-06 00:09:01 -0700

    Make CRC work with KVH's CRC scheme.

--- firmware/atrias2.1/include/crc.h
+++ firmware/atrias2.1/include/crc.h
@@ -7,7 +7,6 @@
 typedef uint32_t crc_t;   /* KVH IMU has 32-bit CRC */
 crc_t crc_table[256];
 
-crc_t reflect(crc_t crc, uint8_t num_bits);
 void crc_generate_table(void);
 crc_t crc_calc(uint8_t *packet, uint8_t num_bytes);
 uint8_t is_packet_good(crc_t my_crc, crc_t kvh_crc);
--- firmware/atrias2.1/src/crc.c
+++ firmware/atrias2.1/src/crc.c
@@ -5,28 +5,10 @@
  * http://www.zorc.breitbandkatze.de/crc.html
  */
 
-crc_t crc_table[256];
 const uint8_t CRC_ORDER = 32;
 const crc_t CRC_HIGHBIT = (crc_t) (1UL << 31);
 const crc_t CRC_POLY = (crc_t) (0x04c11db7);
 const crc_t CRC_INIT = (crc_t) (0xffffffff);
-const crc_t CRC_XOR  = (crc_t) (0xffffffff);
-
-crc_t reflect(crc_t crc, uint8_t num_bits)
-{
-	/*
-	 * Reflect the lower 'num_bits' bits of 'crc'
-	 */
-	crc_t i, j=1, crcout=0;
-
-	for (i=(uint32_t)1<<(num_bits-1); i; i>>=1) {
-		if (crc & i) {
-			crcout |= j;
-		}
-		j <<= 1;
-	}
-	return (crcout);
-}
 
 void crc_generate_table(void)
 {
@@ -39,7 +21,6 @@ void crc_generate_table(void)
 
 	for (dividend=0; dividend<256; dividend++) {
 		remainder = (crc_t) dividend;
-		remainder = reflect(remainder, 8);
 		remainder <<= CRC_ORDER-8;
 
 		for (i=0; i<8; i++) {
@@ -50,7 +31,6 @@ void crc_generate_table(void)
 				remainder = (remainder << 1);
 			}
 		}
-		remainder = reflect(remainder, CRC_ORDER);
 		crc_table[dividend] = remainder;
 	}
 }
@@ -63,14 +43,12 @@ crc_t crc_calc(uint8_t *packet, uint8_t num_bytes)
 	 */
 	crc_t crc = CRC_INIT;
 
-	crc = reflect(crc, CRC_ORDER);
-
 	/* Divide packet by polynomial one byte at a time. */
 	while (num_bytes--) {
-		crc = (crc >> 8) ^ crc_table[ (crc & 0xff) ^ *packet++];
+		crc = (crc << 8) ^ crc_table[ ((crc >> 24) & 0xff) ^ *packet++];
 	}
 
-	return(crc ^ CRC_XOR);
+	return crc;
 }
 
 uint8_t is_packet_good(crc_t my_crc, crc_t kvh_crc)
